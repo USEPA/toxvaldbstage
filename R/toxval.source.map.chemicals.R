997ac306f414051871f13a7c70d92ce66da12aef
jwall01 <wall.jonathan@epa.gov>
2022-05-31 16:07:15 -0400

    Updated join logic for returned curated chemical files

--- R/toxval.source.map.chemicals.R
+++ R/toxval.source.map.chemicals.R
@@ -14,15 +14,17 @@
 #' @import RMySQL dplyr readxl
 #' @export
 #--------------------------------------------------------------------------------------
-toxval.source.map.chemicals <- function(source.db, curated.path){
+toxval.source.map.chemicals <- function(source.db, input.path, curated.path){
   message("Function still in draft phase - waiting for curated chemical files")
   return()
-  # Get source table map files - clean up to match database chemical tables
+  # Get source chemical table name to ID map
   # Repo/chemical_mapping/renamed_source_chemical_files/
   source_table_list = list.files(input.path)
   source_table_list = lapply(source_table_list, function(t){
     t %>%
+      # Remove file extension
       gsub(".xlsx", "", .) %>%
+      # Split by "_"
       strsplit(., split="_") %>%
       unlist() %>%
     # Restructure split string parts into table
@@ -31,29 +33,68 @@ toxval.source.map.chemicals <- function(source.db, curated.path){
                stringsAsFactors = FALSE) %>%
       select(id, source_chem_table) %>%
       distinct() %>%
-      # Remove parentheses from name
+      # Remove parentheses from table name
       mutate(source_chem_table = gsub("[()]", "", source_chem_table) %>%
                gsub("-", "_", .)) %>%
       return()
   }) %>% dplyr::bind_rows()
 
-  # Get curated chemical lists to map
-  #Repo/chemical_mapping/jira_chemical_files
-  curated_list = list.files(curated.path,
-                            pattern="_full.xlsx")
+  # Get curated chemical lists to map - takes 3 file sets per source table
+  #Repo/chemical_mapping/DSSTOX_879
+  c_dirs = list.dirs(curated.path, recursive = FALSE)
+  curated_list = lapply(c_dirs, function(d){
+    tmp = list.files(d)
+    # Remove Windows temp files that start with "~"
+    return(tmp[!grepl("^~", tmp)])
+  }) %T>% { names(.) <- basename(c_dirs) }
 
-  for(c_list in curated_list){
-    chems = readxl::read_xlsx(paste0(curated.path, c_list))
-    tbl_id = c_list %>%
-      gsub("ToxVal", "", .) %>%
-      gsub("_full.xlsx", "", .)
+
+  for(c_list in curated_list$`DSSTox Files`){
+    cat(paste0("...Processing curation file: ", c_list, "\n"))
+    # Get curated chemical list
+    chems = readxl::read_xlsx(paste0(curated.path, "DSSTox Files/", c_list)) %>%
+      dplyr::rename(External_ID = Extenal_ID) %>%
+      select(DSSTox_Source_Record_Id, External_ID, DSSTox_Substance_Id)
+    # Get toxval source table ID from external ID
+    tbl_id = strsplit(chems$External_ID[1], split="_")[[1]][1] %>%
+      gsub("ToxVal", "", .)
+    # Get toxval source chemical table name to query
     src_chem_tbl = source_table_list$source_chem_table[source_table_list$id == tbl_id]
-    # Join chemical table with curated chemicals
-    chem_tbl = runQuery(paste0("SELECT * FROM ", src_chem_tbl), db=source.db) %>%
-      left_join(chems, by=c("chemical_id", "raw_name", "raw_casrn"))
-    # Drop old chemical table
+    # Get chemical table for source
+    chem_tbl = runQuery(paste0("SELECT * FROM ", src_chem_tbl), db=source.db)
+    # Get BIN file information
+    b_file = curated_list$`BIN Files`[grepl(paste0("ToxVal", tbl_id),
+                                            curated_list$`BIN Files`)] %>%
+      paste0(curated.path, "BIN Files/", .) %>%
+      readxl::read_xlsx(path=.)
+    # Get Jira cleaned information (connect BIN to external_id)
+    j_file = curated_list$jira_chemical_files[grepl(paste0("ToxVal", tbl_id, ".xlsx"),
+                                                    curated_list$jira_chemical_files)] %>%
+      paste0(curated.path, "jira_chemical_files/", .) %>%
+      readxl::read_xlsx(path=.) %>%
+      # Match back to curated query which replaced "-" with NA for CASRN
+      mutate(raw_casrn = ifelse(raw_casrn == "-", NA, raw_casrn))
+
+    # Join chemical file information
+    out = chem_tbl %>%
+      # Join to joined Jira and BIN file
+      left_join(j_file %>%
+                  left_join(b_file,
+                            by=c("raw_casrn"="Query Casrn",
+                                 "raw_name"="Query Name")) %>%
+                  select(-raw_casrn, -raw_name),
+                by="chemical_id") %>%
+      left_join(chems, by=c("chemical_id"="External_ID")) %T>% {
+        # Output an intermediate check for incomplete cases to see if join successful
+        out_check <<- filter(., !complete.cases(.))
+      }
+    # If any incomplete cases aren't "No Hits", error stop...
+    if(any(!out_check$`Lookup Result` %in% c("No Hits"))){
+      stop("Error processing ", c_list, "...incomplete join cases found...")
+    }
+    # # Drop old chemical table (we pulled it into the new table to be written)
     # runStatement(paste0("DROP TABLE ", src_chem_tbl), db=source.db)
-    # Write new chemical table
-    #runInsertTable(chem_tbl,src_chem_tbl,source.db,do.halt=TRUE,verbose=FALSE)
+    # # Replace with new chemical table
+    # runInsertTable(chem_tbl,src_chem_tbl,source.db,do.halt=TRUE,verbose=FALSE)
   }
 }
