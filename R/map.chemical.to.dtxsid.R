cb20584834439221666c892238d3dd9bc07a3eae
richardjudson <judson.richard@epa.gov>
2022-06-30 15:53:54 -0400

    optimzie code to speed up

--- R/map.chemical.to.dtxsid.R
+++ R/map.chemical.to.dtxsid.R
@@ -15,32 +15,37 @@ map.chemical.to.dtxsid <- function(toxval.db,source,verbose=T) {
   }
   dsstox = DSSTOX
   rownames(dsstox) = dsstox$casrn
+  names(dsstox)[is.element(names(dsstox),"dsstox_substance_id")] <- "dtxsid"
+
   chems = runQuery(paste0("select distinct chemical_id,cleaned_casrn as casrn from source_chemical where source='",source,"'"),toxval.db)
-  chems$dtxsid = NA
-  for(i in 1:nrow(chems)) {
-    casrn = chems[i,"casrn"]
-    if(is.element(casrn,dsstox$casrn)) {
-      chems[i,"dtxsid"] = dsstox[casrn,"dsstox_substance_id"]
-      chems[i,"casrn"] = casrn
-      chems[i,"name"] = dsstox[casrn,"preferred_name"]
-    }
-    else chems[i,"dtxsid"] = "NODTXSID"
-    if(verbose) if(i%%100==0) cat("dtxsid updated:",i," out of ",nrow(chems),"\n")
+  chems$dtxsid = "NODTXSID"
+  #rownames(chems) = chems$casrn
+  clist = chems$casrn
+  clist = clist[is.element(clist,dsstox$casrn)]
+  dtemp = dsstox[clist,]
+  for(i in 1:length(clist)) {
+    casrn = clist[i]
+    name = dtemp[casrn,"preferred_name"]
+    dtxsid = dtemp[casrn,"dtxsid"]
+    chems[is.element(chems$casrn,casrn),"dtxsid"] = dtxsid
+    chems[is.element(chems$casrn,casrn),"casrn"] = casrn
+    chems[is.element(chems$casrn,casrn),"name"] = name
+    if(verbose) if(i%%500==0) cat("dtxsid updated:",i," out of ",nrow(chems),"\n")
   }
 
+  #runQuery("analyze table source_chemical",toxval.db)
   query = paste0("update source_chemical set casrn=cleaned_casrn, name=cleaned_name where source='",source,"'")
   runQuery(query,toxval.db)
+  #runQuery("analyze table source_chemical",toxval.db)
   for(i in 1:nrow(chems)) {
     chemical_id = chems[i,"chemical_id"]
     casrn = chems[i,"casrn"]
     name = chems[i,"name"]
     dtxsid = chems[i,"dtxsid"]
-    query = paste0("update toxval set dtxsid='",dtxsid,"' where chemical_id='",chemical_id,"'")
-    runQuery(query,toxval.db)
     query = paste0("update source_chemical set dtxsid='",dtxsid,"' where chemical_id='",chemical_id,"'")
     runQuery(query,toxval.db)
-
-
-    if(verbose) if(i%%100==0) cat("chemicals updated:",i," out of ",nrow(chems),"\n")
+    query = paste0("update toxval set dtxsid='",dtxsid,"' where chemical_id='",chemical_id,"'")
+    runQuery(query,toxval.db)
+    if(verbose) if(i%%500==0) cat("chemicals updated:",i," out of ",nrow(chems),"\n")
   }
 }
