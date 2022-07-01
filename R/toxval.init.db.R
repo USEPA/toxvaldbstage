048ae9d51802b32a256f2d7a6e11bf7be6c43dda
richardjudson <judson.richard@epa.gov>
2022-07-01 14:37:49 -0400

    add code to reset the database

--- R/toxval.init.db.R
+++ R/toxval.init.db.R
@@ -3,9 +3,25 @@
 #' @param toxval.db The version of toxval into which the tables are loaded.
 #' @export
 #--------------------------------------------------------------------------------------
-toxval.init.db <- function(toxval.db) {
+toxval.init.db <- function(toxval.db,reset=F) {
   printCurrentFunction(toxval.db)
 
+  if(reset) {
+    exclude.list <- c("source_chemical","toxval","info","source_chemical")
+    table.list <- runQuery("show tables",toxval.db)[,1]
+    table.list <- table.list[!is.element(table.list,exclude.list)]
+    for(table in table.list) {
+      cat("delete from",table,"\n")
+      query <- paste0("delete from ",table)
+      runInsert(query,toxval.db,do.halt=T)
+    }
+    cat("delete from info\n")
+    runInsert("delete from info",toxval.db,do.halt=T)
+    cat("delete from toxval\n")
+    runInsert("delete from toxval",toxval.db,do.halt=T)
+    cat("delete from source_chemical\n")
+    runInsert("delete from source_chemical",toxval.db,do.halt=T)
+  }
   #################################################################
   cat("load the info table\n")
   #################################################################
