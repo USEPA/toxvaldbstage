8879571472b874d8cf5c167abca3dd342d4f5324
richardjudson <judson.richard@epa.gov>
2022-07-01 14:38:15 -0400

    update some clowder_ids that were wrong

--- R/set_clowder_id.R
+++ R/set_clowder_id.R
@@ -32,25 +32,25 @@ set_clowder_id <- function(res,source) {
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="ATSDR MRLs 2020") {
+  else if(source=="ATSDR MRLs 2020") {
     res$clowder_id = "610036c7e4b01a90a3f9879c"
     res$document_name = "a6a427952aa24d5d9e1d1a229109ba7e-Agency for Toxic Substances and Disease Registry-2020.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="ATSDR PFAS") {
+  else if(source=="ATSDR PFAS") {
     res$clowder_id = "6238e943e4b0b18cb57ced5a"
     res$document_name = "tp200-c2.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="ATSDR PFAS 2021") {
+  else if(source=="ATSDR PFAS 2021") {
     res$clowder_id = "6238b97ae4b0b18cb57ce4f6"
     res$document_name = "tp200.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="Chiu") {
+  else if(source=="Chiu") {
     res$clowder_id = "61003953e4b01a90a3f9b6d1"
     res$document_name = "08b44893c3ec2ed2c917bd2962aefca2-Chiu-2018-Beyond the.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
@@ -62,25 +62,25 @@ set_clowder_id <- function(res,source) {
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("DOE Wildlife Benchmarks"))) {
+  else if(is.element(source,c("DOE Wildlife Benchmarks"))) {
     res$clowder_id = "61003756e4b01a90a3f99471"
     res$document_name = "ee2b2fc62d5fa7a1e025dd6a119bdd16-Sample-1996-Toxicologi.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="EnviroTox_v2") {
+  else if(source=="EnviroTox_v2") {
     res$clowder_id = "61f14c70e4b0ebacf2ec476c"
     res$document_name = "Connors_2019a.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("EPA AEGL"))) {
+  else if(is.element(source,c("EPA AEGL"))) {
     res$clowder_id = "61003a84e4b01a90a3f9ca2f"
     res$document_name = "4627c891c8ea494fb8ea7846b220bd14-United States Environmental Protection Agency (USEPA)-2020-Acute Expo.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("EPA OPP"))) {
+  else if(is.element(source,c("EPA OPP"))) {
     res$clowder_id = "610038d7e4b01a90a3f9ada1"
     res$document_name = "44b5bf69be26a74522e947eea7a44e4c-United States Environmental Protection Agency (USEPA)-2017-Huma.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
@@ -92,35 +92,35 @@ set_clowder_id <- function(res,source) {
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("NIOSH"))) {
+  else if(is.element(source,c("NIOSH"))) {
     res$clowder_id="61fabd3de4b04a563fdc9b99"
     res$document_name="ToxValQA33091630_NIOSH_2020_ImmediatelyDangerous-(IDLH)Values.pdf"
     return(res)
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("OW Drinking Water Standards"))) {
+  else if(is.element(source,c("OW Drinking Water Standards"))) {
     res$clowder_id = "610036ede4b01a90a3f98ae0"
     res$document_name = "b5ffe2b7e16578b78213213141cfc3ad-United States Environmental Protection Agency (USEPA)-2018-2018 Drink.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="Pennsylvania DEP MCLs") {
+  else if(source=="Pennsylvania DEP MCLs") {
     res$clowder_id = "61003849e4b01a90a3f9a409"
     res$document_name = "3d84a7d7e81b35b4e5ac9b820558be27-Pennsylvania Department of Environmental Protection (DEP)-2012.pdf "
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="Pennsylvania DEP ToxValues") {
+  else if(source=="Pennsylvania DEP ToxValues") {
     res$clowder_id = "61003849e4b01a90a3f9a409"
     res$document_name = "3d84a7d7e81b35b4e5ac9b820558be27-Pennsylvania Department of Environmental Protection (DEP)-2012.pdf "
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="RSL") {
+  else if(source=="RSL") {
     res[,"clowder_id"] = "61fabdc0e4b04a563fdc9bdd"
     res[,"document_name"] = "ToxValQA33099727_EPA_2021_RegionalScreeningLevels-(TR=1E-06,THQ=1.0).pdf"
-    res[res$risk_assessment_class=="subchronic","clowder_id"] = "61fac10fe4b04a563fdc9c9c"
+    res[res$risk_assessment_class=="subchronic","clowder_id"] = "61fabdc0e4b04a563fdc9be1"
     res[res$risk_assessment_class=="subchronic","document_name"] = "ToxValQA33106078_EPA_2021_RegionalScreeningLevels-SubchronicToxicityValues.pdf"
     res[res$toxval_subtype=="Thq =  1","clowder_id"] = "61fabdc0e4b04a563fdc9bdd"
     res[res$toxval_subtype=="Thq =  1","document_name"] = "ToxValQA33099727_EPA_2021_RegionalScreeningLevels-(TR=1E-06,THQ=1.0).pdf"
@@ -129,37 +129,37 @@ set_clowder_id <- function(res,source) {
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="USGS HBSL") {
+  else if(source=="USGS HBSL") {
     res$clowder_id = "61fabdc3e4b04a563fdc9c0b"
     res$document_name = "ToxValQA29186291_USGS_2018_Health-BasedScreening-Water-QualityData.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(source=="WHO IPCS") {
+  else if(source=="WHO IPCS") {
     res$clowder_id = "61bb6895e4b0d5d4d1e36956"
     res$document_name = "FAOWHO_2008_Pesticideresidues-Report2008.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("OSHA Air contaminants"))) {
+  else if(is.element(source,c("OSHA Air contaminants"))) {
     res$clowder_id="61fabd47e4b04a563fdc9bb8"
     res$document_name="ToxValQA29180809_OSHA_TABLEZ-1LimitsforAirContaminants.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("FDA CEDI"))) {
+  else if(is.element(source,c("FDA CEDI"))) {
     res$clowder_id="619d2972e4b0993a3937de4f"
     res$document_name="ToxValQA29176904_FDA_CumulativeEstimatedDailyIntake.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("Wignall"))) {
-    res$clowder_id="62b3091fe4b07abf29f567eb"
+  else if(is.element(source,c("Wignall"))) {
+    res$clowder_id="62b30a1ee4b07abf29f56811"
     res$document_name="ToxValDBQA Wignall EHP 2014.pdf"
     cat("clowder_id and document_name set for ",source,"\n")
     return(res)
   }
-  if(is.element(source,c("DOD ERED",
+  else if(is.element(source,c("DOD ERED",
                          "HEAST",
                          "IRIS",
                          "PFAS 150 SEM",
@@ -177,7 +177,7 @@ set_clowder_id <- function(res,source) {
   cat("Do the non-easy sources\n")
   ##########################################################
 
-   nlist = names(res)
+  nlist = names(res)
   if(!is.element("clowder_id",nlist)) res$clowder_id = "-"
   if(!is.element("document_name",nlist)) res$document_name = "-"
 
