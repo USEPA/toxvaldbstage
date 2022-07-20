1cdd52df96172acdd8b56287512290f006060352
richardjudson <judson.richard@epa.gov>
2022-07-20 15:13:51 -0400

    comment out selected sources

--- R/import.driver.R
+++ R/import.driver.R
@@ -24,13 +24,13 @@ import.driver <- function(db="res_toxval_source_v5",
     }
   }
 
-  import_envirotox_source(db,chem.check.halt=chem.check.halt)# EnviroTox_v2
-  import_heast_source(db,chem.check.halt=chem.check.halt) # HEAST
-  import_rsl_source(db,chem.check.halt=chem.check.halt) # RSL
-  import_dod_source(db,chem.check.halt=chem.check.halt) # DOD [v1]
-  import_doe_source(db,chem.check.halt=chem.check.halt) # DOE Protective Action Criteria [v1]
-  import_oppt_source(db,chem.check.halt=chem.check.halt) # EPA OPPT
-  import_pfas_150_sem_source(db,chem.check.halt=chem.check.halt) # PFAS 150 SEM
+  #import_envirotox_source(db,chem.check.halt=chem.check.halt)# EnviroTox_v2
+  #import_heast_source(db,chem.check.halt=chem.check.halt) # HEAST
+  #import_rsl_source(db,chem.check.halt=chem.check.halt) # RSL
+  #import_dod_source(db,chem.check.halt=chem.check.halt) # DOD [v1]
+  #import_doe_source(db,chem.check.halt=chem.check.halt) # DOE Protective Action Criteria [v1]
+  #import_oppt_source(db,chem.check.halt=chem.check.halt) # EPA OPPT
+  #import_pfas_150_sem_source(db,chem.check.halt=chem.check.halt) # PFAS 150 SEM
   import_atsdr_pfas_2021_source(db,chem.check.halt=chem.check.halt) # ATSDR PFAS 2021 [v1]
   import_atsdr_pfas_source(db,chem.check.halt=chem.check.halt) # ATSDR PFAS [v1]
   import_atsdr_source(db,chem.check.halt=chem.check.halt) # ATSDR MRLs 2020 [v1]
@@ -40,6 +40,7 @@ import.driver <- function(db="res_toxval_source_v5",
   import_cosmos_source(db,chem.check.halt=chem.check.halt) # COSMOS [v1]
   import_dod_ered_source(db,chem.check.halt=chem.check.halt) # DOD ERED [v1]
   import_doe_benchmarks_source(db,chem.check.halt=chem.check.halt) # DOE Wildlife Benchmarks [v1]
+  import_echa_echemportal_api_source(db,chem.check.halt=chem.check.halt) # ECHA echemportal API
   import_efsa2_source(db,chem.check.halt=chem.check.halt)  # EFSA2
   import_efsa_source(db,chem.check.halt=chem.check.halt) # EFSA
   import_hawc_pfas_150_source(db,chem.check.halt=chem.check.halt) # HAWC PFAS 150
@@ -58,7 +59,6 @@ import.driver <- function(db="res_toxval_source_v5",
   import_pprtv_ornl_source(db,chem.check.halt=chem.check.halt) # PPRTV (ORNL)
   import_test_source(db,chem.check.halt=chem.check.halt) # TEST
   import_wignall_source(db,chem.check.halt=chem.check.halt) # Wignall
-
   # Alaska DEC [v1]
   # Cal DPH [v1]
   # EPA AEGL [v1]
@@ -72,7 +72,6 @@ import.driver <- function(db="res_toxval_source_v5",
   import_flex_source(db,chem.check.halt=chem.check.halt)
 
   ###import_echa3_source(db,chem.check.halt=chem.check.halt) # ECHA
-  ###import_echa_echemportal_api_source(db,chem.check.halt=chem.check.halt) # ECHA echemportal API
   ###import_echa_echemportal_source(db,chem.check.halt=chem.check.halt) # ECHA eChemPortal 2020
   ###import_echa_iuclid_source(db,chem.check.halt=chem.check.halt) # ECHA IUCLID
   ###import_echa_source(db,chem.check.halt=chem.check.halt) # ECHA POC
