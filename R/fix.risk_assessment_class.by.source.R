e5fb494ea7242e45248ca3d6d088289f89877b44
richardjudson <judson.richard@epa.gov>
2022-07-20 15:07:59 -0400

    update the dictionary used

--- R/fix.risk_assessment_class.by.source.R
+++ R/fix.risk_assessment_class.by.source.R
@@ -7,97 +7,136 @@
 #' @param restart If TRUE, delete all values and start from scratch
 #' @export
 #--------------------------------------------------------------------------------------
-fix.risk_assessment_class.by.source <- function(toxval.db,source, restart=F,add.rules=T) {
+fix.risk_assessment_class.by.source <- function(toxval.db,source, restart=T) {
   printCurrentFunction(paste(toxval.db,":", source))
-  conv = read.xlsx(paste0(toxval.config()$datapath,"dictionary/RAC_rules.xlsx"))
+  conv = read.xlsx(paste0(toxval.config()$datapath,"dictionary/RAC_rules_by_source v92.xlsx"))
   print(dim(conv))
   conv = conv[conv$order>0,]
   conv = conv[order(conv$term),]
   conv = conv[order(conv$risk_assessment_class),]
   conv = conv[order(conv$order),]
-  file = paste0(toxval.config()$datapath,"dictionary/RAC_rules_by_source.xlsx")
-  rac.src = read.xlsx(file)
-  row = rac.src[1,]
-  row[] = NA
+  conv = conv[conv$useme==1,]
+  conv = conv[!is.na(conv$source),]
+  # file = paste0(toxval.config()$datapath,"dictionary/RAC_rules_by_source.xlsx")
+  # rac.src = read.xlsx(file)
+  # row = rac.src[1,]
+  # row[] = NA
   if(restart) {
     query = paste0("update toxval set risk_assessment_class = '-'  where source like '",source,"'")
     runInsert(query,toxval.db,T,F,T)
   }
-
-  if(source=="TEST") {
-    rac = "acute"
-    query = paste0("update toxval set risk_assessment_class = '",rac,"' where source = '",source,"'")
+  dict = conv[conv$source==source,]
+  n1.0 = runQuery(paste0("select count(*) from toxval where risk_assessment_class='-' and source = '",source,"'") ,toxval.db)[1,1]
+  for(i in 1:nrow(dict)){
+    term = dict[i,"term"]
+    rac = dict[i,"risk_assessment_class"]
+    field = dict[i,"field"]
+    source_c = dict[i,"source"]
+    order = dict[i,"order"]
+    query = paste0("update toxval set risk_assessment_class = '",rac,"' where ",field," = '",term,"' and source = '",source,"' and risk_assessment_class='-'")
     runInsert(query,toxval.db,T,F,T)
+    n0 = runQuery(paste0("select count(*) from toxval where source = '",source,"'"),toxval.db )[1,1]
+    n1 = runQuery(paste0("select count(*) from toxval where risk_assessment_class='-' and source = '",source,"'") ,toxval.db)[1,1]
+    cat("RAC still missing: ",order," : ",n1," out of ",n0," from original:",field,":",term," to rac:",rac,"\n")
+    if(n1==0) break()
   }
-  else if(is.element(source,conv$source)) {
-    dict = conv[conv$source==source,]
-    for(i in 1:nrow(dict)) {
-      term = dict[i,"term"]
-      rac = dict[i,"risk_assessment_class"]
-      field = dict[i,"field"]
-      order = dict[i,"order"]
-      query = paste0("update toxval set risk_assessment_class = '",rac,"' where ",field," = '",term,"' and source = '",source,"' and risk_assessment_class='-'")
-      runInsert(query,toxval.db,T,F,T)
-    }
-  }
-  # if(source == "DOD"){
-  #   cat("deal with DOD\n")
-  #   dict = conv[conv$source=="DOD",]
-  #   for(i in 1:nrow(dict)) {
-  #     term <- dict[i,"term"]
-  #     rac <- dict[i,"risk_assessment_class"]
-  #     field <- dict[i,"field"]
-  #     #Source <- dict[i,"source"]
-  #     order = dict[i,"order"]
-  #     query <- paste0("update toxval set risk_assessment_class = '",rac,"' where ",field," = '",term,"' and source = '",source,"' and risk_assessment_class='-'")
-  #     runInsert(query,toxval.db,T,F,T)
-  #   }
-  # }
-  else if(source == "ECOTOX"){
-    cat("deal with ECOTOX\n")
-    st.list = runQuery("select distinct study_type from toxval where source='ECOTOX'",toxval.db)[,1]
-    sdc.list = runQuery("select distinct study_duration_class from toxval where source='ECOTOX'",toxval.db)[,1]
-    for(st in st.list) {
-      for(sdc in sdc.list) {
-        rac = paste(sdc,tolower(st))
-        cat("ecotox ",st,sdc,rac,"\n")
-        query = paste0("update toxval set risk_assessment_class = '",rac,"' where source = 'ECOTOX' and
-                      study_type = '",st,"' and study_duration_class = '",sdc,"' and risk_assessment_class='-'")
-        runInsert(query,toxval.db,T,F,T)
-        n0 = runQuery("select count(*) from toxval",toxval.db)[1,1]
-        n1 = runQuery("select count(*) from toxval where risk_assessment_class='-'",toxval.db)[1,1]
-        cat("RAC still missing: ",n1," out of ",n0," : ",rac,"\n")
-      }
-    }
-  }
-  else {
-    n1.0 = 0
-    for(i in 1:nrow(conv)){
-      term = conv[i,"term"]
-      rac = conv[i,"risk_assessment_class"]
-      field = conv[i,"field"]
-      source_c = conv[i,"source"]
-      order = conv[i,"order"]
-      query = paste0("update toxval set risk_assessment_class = '",rac,"' where ",field," = '",term,"' and source = '",source,"' and risk_assessment_class='-'")
-      runInsert(query,toxval.db,T,F,T)
-      n0 = runQuery(paste0("select count(*) from toxval where source = '",source,"'"),toxval.db )[1,1]
-      n1 = runQuery(paste0("select count(*) from toxval where risk_assessment_class='-' and source = '",source,"'") ,toxval.db)[1,1]
-      cat("RAC still missing: ",order," : ",n1," out of ",n0," from original:",field,":",term," to rac:",rac,"\n")
-      if(n1==0) break()
-      if(n1.0==n1 && restart==T) n1.0 = n1
-      else {
-        row[] = NA
-        row[1,"term"] = term
-        row[1,"source"] = source
-        row[1,"field"] = field
-        row[1,"risk_assessment_class"] = rac
-        row[1,"order"] = order
-        rac.src = rbind(rac.src,row)
-      }
-    }
-  }
-  if(add.rules) {
-    file = paste0(toxval.config()$datapath,"dictionary/RAC_rules_by_source.xlsx")
-    write.xlsx(rac.src,file)
+  if(n1>0) {
+    query = paste0("SELECT
+                    b.toxval_id,b.source_hash,b.source_table,
+                    a.dtxsid,a.casrn,a.name,b.chemical_id,
+                    b.source,b.subsource,
+                    b.source_url,b.subsource_url,
+                    b.qc_status,
+                    b.details_text,
+                    b.priority_id,
+                    b.risk_assessment_class,
+                    b.human_eco,
+                    b.toxval_type,b.toxval_type_original,
+                    b.toxval_subtype,
+                    e.toxval_type_supercategory,
+                    e.toxval_type_category,
+                    b.toxval_numeric,b.toxval_units,
+                    b.toxval_numeric_original,b.toxval_units_original,
+                    b.toxval_numeric_standard,b.toxval_units_standard,
+                    b.toxval_numeric_human,b.toxval_units_human,
+                    b.study_type,b.study_type_original,
+                    b.study_duration_class,b.study_duration_class_original,
+                    b.study_duration_value,b.study_duration_value_original,
+                    b.study_duration_units,b.study_duration_units_original,
+                    b.species_id,b.species_original,d.common_name,d.latin_name,d.ecotox_group,d.habitat,
+                    b.strain,b.strain_group,b.strain_original,
+                    b.sex,b.sex_original,
+                    b.generation,b.lifestage,
+                    b.exposure_route,b.exposure_route_original,
+                    b.exposure_method,b.exposure_method_original,
+                    b.exposure_form,b.exposure_form_original,
+                    b.media,b.media_original,
+                    b.critical_effect,
+                    b.critical_effect_original,
+                    b.year,
+                    b.datestamp,
+                    f.long_ref,
+                    f.title,
+                    f.author,
+                    f.journal,
+                    f.volume,
+                    f.year,
+                    f.issue,
+                    f.url,
+                    f.document_name
+                    FROM
+                    toxval b
+                    INNER JOIN chemical a on a.dtxsid=b.dtxsid
+                    LEFT JOIN species d on b.species_id=d.species_id
+                    INNER JOIN toxval_type_dictionary e on b.toxval_type=e.toxval_type
+                    INNER JOIN record_source f on b.toxval_id=f.toxval_id
+                    WHERE
+                    b.source='",source,"'
+                   and risk_assessment_class='-'")
+
+    query = paste0("SELECT
+                    b.toxval_id,b.source_hash,b.source_table,
+                    a.dtxsid,a.casrn,a.name,b.chemical_id,
+                    b.source,b.subsource,
+                    b.source_url,b.subsource_url,
+                    b.qc_status,
+                    b.details_text,
+                    b.priority_id,
+                    b.risk_assessment_class,
+                    b.human_eco,
+                    b.toxval_type,b.toxval_type_original,
+                    b.toxval_subtype,
+                    b.toxval_numeric,b.toxval_units,
+                    b.toxval_numeric_original,b.toxval_units_original,
+                    b.toxval_numeric_standard,b.toxval_units_standard,
+                    b.toxval_numeric_human,b.toxval_units_human,
+                    b.study_type,b.study_type_original,
+                    b.study_duration_class,b.study_duration_class_original,
+                    b.study_duration_value,b.study_duration_value_original,
+                    b.study_duration_units,b.study_duration_units_original,
+                    b.species_id,b.species_original,
+                    b.strain,b.strain_group,b.strain_original,
+                    b.sex,b.sex_original,
+                    b.generation,b.lifestage,
+                    b.exposure_route,b.exposure_route_original,
+                    b.exposure_method,b.exposure_method_original,
+                    b.exposure_form,b.exposure_form_original,
+                    b.media,b.media_original,
+                    b.critical_effect,
+                    b.critical_effect_original,
+                    b.year,
+                    b.datestamp
+                    FROM
+                    toxval b
+                    INNER JOIN chemical a on a.dtxsid=b.dtxsid
+                    WHERE
+                    b.source='",source,"'
+                   and risk_assessment_class='-'")
+
+
+    temp = runQuery(query,toxval.db)
+    if(!is.element(source,c("DOD ERED","EFSA","HAWC","HPVIS","IRIS"))) browser()
+    file = paste0(toxval.config()$datapath,"missing_rac/missing_",source,".xlsx")
+    write.xlsx(temp,file)
   }
 }
