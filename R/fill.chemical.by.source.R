4f5d5084d3bd9892ab7192aec428fa899322bebd
richardjudson <judson.richard@epa.gov>
2022-07-26 07:50:35 -0400

    code cleanup

--- R/fill.chemical.by.source.R
+++ R/fill.chemical.by.source.R
@@ -8,24 +8,27 @@
 fill.chemical.by.source <- function(toxval.db, source, verbose=T) {
   printCurrentFunction(paste(toxval.db,":", source))
 
-  runQuery(paste0("delete from chemical where dtxsid in (select dtxsid from source_chemical where source = '",source,"')"),toxval.db)
-  res <- runQuery(paste0("select dtxsid,casrn,name from source_chemical where source = '",source,"'") ,toxval.db)
-  res <- res[!is.element(res$dtxsid,"-"),]
+  #runQuery(paste0("delete from chemical where dtxsid in (select dtxsid from source_chemical where source = '",source,"')"),toxval.db)
+  res = runQuery(paste0("select dtxsid,casrn,name from source_chemical where source = '",source,"'") ,toxval.db)
+  res = res[!is.element(res$dtxsid,"-"),]
   cat("Rows in source_chemical:",nrow(res),"\n")
-  res <- unique(res)
+  res = unique(res)
   cat("Unique rows in source_chemical:",nrow(res),"\n")
-  x <- unique(res$dtxsid)
+  x = unique(res$dtxsid)
   cat("Unique dtxsid values:",length(x),"\n")
-  res <- res[order(res$dtxsid),]
-  res2 <- res
+  res = res[order(res$dtxsid),]
+  res2 = res
   res2$duplicate <- 0
   for(i in 2:nrow(res2)) {
     if(res2[i,"dtxsid"]==res2[i-1,"dtxsid"]) {
       res2[i,"duplicate"] <- 1
     }
   }
-  res2 <- res2[res2$duplicate==0,]
-  cat("Unique rows in res2:",nrow(res2),"\n")
-  res2 <- res2[,c("dtxsid","casrn","name")]
+  res2 = res2[res2$duplicate==0,]
+  dlist = runQuery("select distinct dtxsid from chemical",toxval.db)
+  res2 = res2[!is.element(res2$dtxsid,dlist),]
+
+  cat("Unique rows in res2 to be added:",nrow(res2),"\n")
+  res2 = res2[,c("dtxsid","casrn","name")]
   runInsertTable(res2, "chemical", toxval.db, verbose)
 }
