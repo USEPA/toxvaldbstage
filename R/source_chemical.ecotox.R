506f181b24f6b047c0d43353f90b7f28f6f83a90
richardjudson <judson.richard@epa.gov>
2022-07-26 07:54:04 -0400

    add comments, change process to deal with DSSTOX

--- R/source_chemical.ecotox.R
+++ R/source_chemical.ecotox.R
@@ -1,23 +1,22 @@
 #--------------------------------------------------------------------------------------
 #' special process to deal with source chemicals for ECOTOX
-#' @param db The version of toxval into which the source info is loaded.
-#' @param infile1 The input file ./test/test_files/TEST data.xlsx
-#' @param infile2 The input file ./test/test_files/test_chemicals_invitrodb.csv to map casrn to names from prod_internal_invitrodb_v3_2.chemical
+#' @param toxval.db The version of toxval into which the source info is loaded.
+#' @param source.db The source database version
+#' @param chem.check.halt If TRUE, halt if there are errors in the chemical checking
+#' @param casrn.col  Name of the column containing the CASRN
+#' @param name.col Name of the column containing chemical names
+#' @param verbose If TRUE, output extra diagnostics information
 #--------------------------------------------------------------------------------------
 source_chemical.ecotox <- function(toxval.db,source.db,
-                                     res,
-                                     source,
-                                     chem.check.halt=FALSE,
-                                     casrn.col="casrn",
-                                     name.col="name",
-                                     verbose=F) {
+                                   res,
+                                   source,
+                                   chem.check.halt=FALSE,
+                                   casrn.col="casrn",
+                                   name.col="name",
+                                   verbose=F) {
   printCurrentFunction(paste0(db,"\n",source))
-  if(!exists("DSSTOX")) {
-    sys.date <- "2021-08-12"
-    file <- paste0("../DSSTox/DSSTox_",sys.date,".RData")
-    load(file)
-    DSSTOX <<- DSSTOX
-  }
+  if(!exists("DSSTOX")) load.dsstox()
+
   #####################################################################
   cat("Do the chemical checking\n")
   #####################################################################
@@ -50,21 +49,32 @@ source_chemical.ecotox <- function(toxval.db,source.db,
   }
   chems$chemical_index = paste(chems$raw_casrn,chems$raw_name)
   cat("add the dtxsid\n")
-  chems$dtxsid = "NODTXSID"
-  chems$casrn = chems$cleaned_name
-  chems$name = chems$cleaned_casrn
+  file = paste0(toxval.config()$datapath,"/ecotox/ecotox_files/ECOTOX chemical mapping 2022-07-21.xlsx")
+  echems = read.xlsx(file)
+  rownames(echems) = as.character(echems$casrn)
   dsstox = DSSTOX[is.element(DSSTOX$casrn,chems$cleaned_casrn),]
+
+  chems$dtxsid = "NODTXSID"
   for(i in 1:nrow(chems)) {
-    casrn = chems[i,"cleaned_casrn"]
-    chems[i,"casrn"] = casrn
-    if(is.element(casrn,dsstox$casrn)) {
-      chems[i,"name"] = dsstox[is.element(dsstox$casrn,casrn),"preferred_name"]
-      chems[i,"dtxsid"] = dsstox[is.element(dsstox$casrn,casrn),"dsstox_substance_id"]
+    casrn = as.character(chems[i,"raw_casrn"])
+    ccasrn = chems[i,"cleaned_casrn"]
+    if(is.element(casrn,echems$casrn)) {
+      chems[i,"cleaned_name"] = echems[casrn,"name"]
+      chems[i,"name"] = echems[casrn,"name"]
+      chems[i,"casrn"] = ccasrn
+      chems[i,"dtxsid"] = echems[casrn,"dtxsid"]
     }
-    if(i%%1000==0) cat(" finished ",i," out of ",nrow(chems),"\n")
+    else {
+      if(is.element(ccasrn,dsstox$casrn)) {
+        chems[i,"cleaned_name"] = dsstox[ccasrn,"preferred_name"]
+        chems[i,"casrn"] = ccasrn
+        chems[i,"name"] = dsstox[ccasrn,"preferred_name"]
+        chems[i,"dtxsid"] = dsstox[ccasrn,"dtxsid"]
+      }
+    }
   }
 
-  cat("add the chemcial_id to res\n")
+  cat("add the chemical_id to res\n")
   res$chemical_id = NA
   for(i in 1:nrow(chems)) {
     indx=chems[i,"chemical_index"];
@@ -73,7 +83,7 @@ source_chemical.ecotox <- function(toxval.db,source.db,
     if(i%%1000==0) cat(" finished ",i," out of ",nrow(chems),"\n")
   }
   chems = subset(chems,select=-c(chemical_index))
-  cids = runQuery(paste0("select distinct chemical_id from source_chemical where source='",source,"'"),toxval.db)[,1]
+  cids = runQuery(paste0("select distinct chemical_id from source_chemical where source='",source,"'"),source.db)[,1]
   chems.new = chems[!is.element(chems$chemical_id,cids),]
   n0 = length(cids)
   n1 = nrow(chems)
@@ -83,6 +93,7 @@ source_chemical.ecotox <- function(toxval.db,source.db,
   cat(source,"\n")
   cat("chem matching: original,new,match:",n0,n1,n01," new percent: ",format(newfrac,digits=2),"\n")
   cat("**************************************************************************\n")
+
   runInsertTable(chems.new,"source_chemical",source.db,do.halt=T,verbose=F)
   return(res)
 }
