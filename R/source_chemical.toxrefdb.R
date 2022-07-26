058238a8606b1cc555eba7fb8ec677943351b2b3
richardjudson <judson.richard@epa.gov>
2022-07-26 07:54:22 -0400

    add comments

--- R/source_chemical.toxrefdb.R
+++ R/source_chemical.toxrefdb.R
@@ -1,24 +1,24 @@
 #--------------------------------------------------------------------------------------
-#' special process to deal with source chemicals for ToxRefDB
-#' @param db The version of toxval into which the source info is loaded.
-#' @param infile1 The input file ./test/test_files/TEST data.xlsx
-#' @param infile2 The input file ./test/test_files/test_chemicals_invitrodb.csv to map casrn to names from prod_internal_invitrodb_v3_2.chemical
+#' Special process to deal with source chemicals for ToxRefDB. This will put the
+#' chemicals into the source database source_chemical table
+#' @param toxval.db The version of toxval into which the source info is loaded.
+#' @param source.db The source database version
+#' @param res The dataframe to which the chemical_id sill be added
+#' @param source The name of the source
+#' @param chem.check.halt If TRUE, stop if there are problems with the chemical mapping
+#' @param casrncol The name of the column containing the CASRN
+#' @param name.col The name ofhte column containing hte chemical name
+#' @param verbose If TRUE, write out diagnostic messages
+#' @return Returns the input dataframe with the chemical_id added
 #--------------------------------------------------------------------------------------
 source_chemical.toxrefdb <- function(toxval.db,source.db,
-                                    res,
-                                    source,
-                                    chem.check.halt=FALSE,
-                                    casrn.col="casrn",
-                                    name.col="name",
-                                    verbose=F) {
+                                     res,
+                                     source="ToxRefDB",
+                                     chem.check.halt=FALSE,
+                                     casrn.col="casrn",
+                                     name.col="name",
+                                     verbose=F) {
   printCurrentFunction(paste0(db,"\n",source))
-  if(!exists("DSSTOX")) {
-    sys.date <- "2021-08-12"
-    file <- paste0("../DSSTox/DSSTox_",sys.date,".RData")
-    load(file)
-    DSSTOX <<- DSSTOX
-  }
-
   #####################################################################
   cat("Do the chemical checking\n")
   #####################################################################
@@ -29,8 +29,8 @@ source_chemical.toxrefdb <- function(toxval.db,source.db,
   #####################################################################
   cat("Build the chemical table\n")
   #####################################################################
-  chems = cbind(res[,c(casrn.col,name.col)],result$res0[,c(casrn.col,name.col)])
-  names(chems) = c("raw_casrn","raw_name","cleaned_casrn","cleaned_name")
+  chems = cbind(res[,c("dtxsid",casrn.col,name.col)],result$res0[,c(casrn.col,name.col)])
+  names(chems) = c("dtxsid","raw_casrn","raw_name","cleaned_casrn","cleaned_name")
   chems = unique(chems)
   chems$source = source
   prefix = runQuery(paste0("select chemprefix from chemical_source_index where source='",source,"'"),source.db)[1,1]
@@ -49,24 +49,13 @@ source_chemical.toxrefdb <- function(toxval.db,source.db,
     browser()
   }
   chems$chemical_index = paste(chems$raw_casrn,chems$raw_name)
-  cat("add the dtxsid\n")
-  chems$dtxsid = "NODTXSID"
-  chems$casrn = chems$cleaned_name
-  chems$name = chems$cleaned_casrn
-  dsstox = DSSTOX[is.element(DSSTOX$casrn,chems$cleaned_casrn),]
-  for(i in 1:nrow(chems)) {
-    casrn = chems[i,"cleaned_casrn"]
-    chems[i,"casrn"] = casrn
-    if(is.element(casrn,dsstox$casrn)) {
-      chems[i,"name"] = dsstox[is.element(dsstox$casrn,casrn),"preferred_name"]
-      chems[i,"dtxsid"] = dsstox[is.element(dsstox$casrn,casrn),"dsstox_substance_id"]
-    }
-  }
+  chems$casrn = chems$cleaned_casrn
+  chems$name = chems$cleaned_name
 
   res$chemical_id = NA
   for(i in 1:nrow(chems)) {indx=chems[i,"chemical_index"]; cid=chems[i,"chemical_id"];res[is.element(res$chemical_index,indx),"chemical_id"]=cid}
   chems = subset(chems,select=-c(chemical_index))
-  cids = runQuery(paste0("select distinct chemical_id from source_chemical where source='",source,"'"),toxval.db)[,1]
+  cids = runQuery(paste0("select distinct chemical_id from source_chemical where source='",source,"'"),source.db)[,1]
   chems.new = chems[!is.element(chems$chemical_id,cids),]
   n0 = length(cids)
   n1 = nrow(chems)
@@ -76,6 +65,7 @@ source_chemical.toxrefdb <- function(toxval.db,source.db,
   cat(source,"\n")
   cat("chem matching: original,new,match:",n0,n1,n01," new percent: ",format(newfrac,digits=2),"\n")
   cat("**************************************************************************\n")
+  #browser()
   runInsertTable(chems.new,"source_chemical",source.db,do.halt=T,verbose=F)
   return(res)
 }
