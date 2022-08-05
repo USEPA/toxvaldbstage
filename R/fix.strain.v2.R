a97494dd903c7c5f43909f47917b99f892ee6c25
richardjudson <judson.richard@epa.gov>
2022-08-05 11:35:23 -0400

    add code to run throuhg all sources

--- R/fix.strain.v2.R
+++ R/fix.strain.v2.R
@@ -3,31 +3,35 @@
 #' Set the strain information in toxval
 #'
 #' @param toxval.db The version of the database to use
-#' @param source The source to be fixed
-#' @param date_string The date ofhte latest dictinary version
+#' @param source The source to be fixed. If NULL, fix for all sources
+#' @param date_string The date of the latest dictionary version
 #' @export
 #--------------------------------------------------------------------------------------
-fix.strain.v2 <- function(toxval.db,source,date_string="2022-05-25") {
+fix.strain.v2 <- function(toxval.db,source=NULL,date_string="2022-08-04") {
   printCurrentFunction()
   file = paste0(toxval.config()$datapath,"species/strain_dictionary_",date_string,".xlsx")
   dict = read.xlsx(file)
 
-  so = runQuery(paste0("select distinct strain_original from toxval where source='",source,"'"),toxval.db)[,1]
-  count.good = 0
-  cat("Start setting strain\n")
-  if(length(so)>0) {
-    for(i in 1:length(so)) {
-      tag = stri_escape_unicode(so[i])
-      tag = str_replace_all(tag,"\\'","")
-      if(is.element(tag,dict$strain_original)) {
-        strain = dict[is.element(dict$strain_original,tag),"strain"]
-        strain = stri_escape_unicode(strain)
-        strain = str_replace_all(strain,"\\'","")
-        sg = dict[is.element(dict$strain_original,tag),"strain_group"]
-        query = paste0("update toxval set strain='",strain,"', strain_group='",sg,"' where source='",source,"' and strain_original='",tag,"'")
-        runQuery(query,toxval.db)
+  if(is.null(source)) slist = runQuery("select distinct source from toxval",toxval.db)[,1]
+  else slist = source
+  for(source in slist) {
+    cat("fix strain:",source,"\n")
+    so = runQuery(paste0("select distinct strain_original from toxval where source='",source,"'"),toxval.db)[,1]
+    count.good = 0
+    if(length(so)>0) {
+      for(i in 1:length(so)) {
+        tag = stri_escape_unicode(so[i])
+        tag = str_replace_all(tag,"\\'","")
+        if(is.element(tag,dict$strain_original)) {
+          strain = dict[is.element(dict$strain_original,tag),"strain"]
+          strain = stri_escape_unicode(strain)
+          strain = str_replace_all(strain,"\\'","")
+          sg = dict[is.element(dict$strain_original,tag),"strain_group"]
+          query = paste0("update toxval set strain='",strain,"', strain_group='",sg,"' where source='",source,"' and strain_original='",tag,"'")
+          runQuery(query,toxval.db)
+        }
+        if(i%%100==0) cat("finished",i,"out of",length(so),":",count.good,"\n")
       }
-      if(i%%100==0) cat("finished",i,"out of",length(so),":",count.good,"\n")
     }
   }
 }
