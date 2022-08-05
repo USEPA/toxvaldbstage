c02bc0f4253fd5fb1fb5985906daca7cad153e95
richardjudson <judson.richard@epa.gov>
2022-08-05 10:09:13 -0400

    Initial commit

---
+++ R/import_generic_source.R
@@ -0,0 +1,33 @@
+#--------------------------------------------------------------------------------------
+#' A generic tmpalte for adding data to toxval_source for a new source
+#'
+#' @param db The version of toxval_source into which the source is loaded.
+#' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
+#--------------------------------------------------------------------------------------
+import_generic_source <- function(db,chem.check.halt=F) {
+  printCurrentFunction(db)
+  source = "name of the source"
+  source_table = "source_{source}"
+  dir = paste0(toxval.config()$datapath,"{source}/{source}_files/")
+  file = paste0(dir,"name of the source file.xlsx")
+  res0 = read.xslx(file)
+  #####################################################################
+  cat("Do any non-generic steps to get the data ready \n")
+  #####################################################################
+  #
+  # the final file should have column names that include "name" and "casrn"
+  # additionally, the names in res need to match names in the source 
+  # database table. You do not need to add any of the generic columns
+  # described in the SOP - they will get added in source_prep_and_load
+  #
+  res = source.specific.transformations(res0)
+
+  #####################################################################
+  cat("Prep and load the data\n")
+  #####################################################################
+  source_prep_and_load(db,source=source,table=source_table,res=res,F,T,T)
+}
+
+
+
+
