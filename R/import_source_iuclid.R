e929469973504969bc48bef4914d3b7151633ee8
jwilli29 <williams.joe.h@epa.gov>
2022-10-07 10:04:45 -0400

    Combined orchestrate_ and import_source_iuclid scripts

--- R/import_source_iuclid.R
+++ R/import_source_iuclid.R
@@ -17,3 +17,26 @@ import_source_iuclid <- function(db, subf, chem.check.halt=F) {
   #####################################################################
   source_prep_and_load(db,source="IUCLID",table=paste0("source_IUCLID_",subf),res=res,F,T,T)
 }
+
+#--------------------------------------------------------------------------------------
+#' Load the various IUCLID subsources into ToxVal
+#' @param dir directory containing the various IUCLID subsource subdirectories
+#' @param db The version of toxval_source into which the source is loaded.
+#' @param do.insert If TRUE, insert data into the database, default TRUE
+#' @param chem.check.halt If TRUE, stop the execution if there are errors in the
+#' @return None, subsources loaded
+#--------------------------------------------------------------------------------------
+
+orchestrate_import_source_iuclid <- function(dir) {
+  # Set the correct directory -- could be changed later
+  dir <- "/ccte/ACToR1/ToxValDB9/Repo/iuclid"
+  # Ensure the script is run in the correct directory
+  if (getwd() != dir) {
+    stop("Working directory must be ", dir)
+  }
+  # Loop through all subdirectories of current wd and load the source files within into ToxVal
+  subdirs <- list.files()
+  for (subf in subdirs) {
+    import_source_iuclid(db, subf, chem.check.halt = F)
+  }
+}
