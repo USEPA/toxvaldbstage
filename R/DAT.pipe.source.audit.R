a6b804721f60a346f4dcf13a7f1876e6ed9f5510
Taylor Wall <wall.jonathan@epa.gov>
2022-10-17 13:59:26 -0400

    Updated final steps for DAT > ToxVal audit with better UPDATE INNER JOIN runUpdate() and column renaming/selection

--- R/DAT.pipe.source.audit.R
+++ R/DAT.pipe.source.audit.R
@@ -14,9 +14,9 @@ DAT.pipe.source.audit <- function(source, db) {
   # Insert logic to pull associated audit files for an input source table
 
   DAT_data = list(
-    live_dat = "Repo/DAT reports/dataset_detail_QC_2022-09-07.xlsx" %>%
+    live_dat = "Repo/DAT reports/dataset_detail_QC_2022-10-17_test.xlsx" %>%
       readxl::read_xlsx(),
-  audit_dat = "Repo/DAT reports/dataset_detail_audit_QC_2022-09-07.xlsx" %>%
+  audit_dat = "Repo/DAT reports/dataset_detail_audit_QC_2022-10-17_test.xlsx" %>%
     readxl::read_xlsx() %>%
     dplyr::rename(src_tbl_name=dataset_name) %>%
     mutate(src_tbl_name = gsub("toxval_", "", src_tbl_name))
@@ -56,33 +56,50 @@ DAT.pipe.source.audit <- function(source, db) {
     select(any_of(id_list), record)
 
   # Correct version numbers based on parent hash version in toxval_source
-  v_list = runQuery(paste0("SELECT parent_hash, version as parent_version FROM ",
+  v_list = runQuery(paste0("SELECT source_hash, version as parent_version FROM ",
                            audit$src_tbl_name %>% unique()), db)
   # Based on toxval_source parent_hash, increment up
   audit = audit %>%
-    left_join(v_list, by="parent_hash") %>%
+    left_join(v_list, by=c("parent_hash" = "source_hash")) %>%
     mutate(audit_version = version + parent_version) %>%
     select(-parent_version, -version)
   # Based on toxval_source parent_hash, increment up
   live = live %>%
-    left_join(v_list, by=c("parent_hash")) %>%
+    left_join(v_list, by=c("parent_hash" = "source_hash")) %>%
     mutate(audit_version = version + parent_version) %>%
     select(-parent_version, -version)
 
   # Determine qc_status
+  live = live %>%
+    mutate(qc_status = "TBD in Code")
 
   # Prep audit table push
   audit = audit %>%
     dplyr::rename(fk_source_hash = source_hash,
                   version = audit_version,
                   qc_notes = data_record_annotation,
-                  qc_flags = failure_reason) %>%
+                  qc_flags = failure_reason,
+                  created_by = create_by) %>%
     select(-status_name)
 
-  # Filter out unchanged records from live table
+  # Filter out unchanged records from live table (unchanged = matching source_hash)
+  # Rename columns as needed
+  live = live %>%
+    filter(!source_hash %in% v_list$source_hash) %>%
+    dplyr::rename(version = audit_version,
+                  qc_notes = data_record_annotation,
+                  qc_flags = failure_reason,
+                  created_by = create_by) %>%
+    select(-dataset_name, -status_name, -source_name, -create_time)
 
   # Push live and audit table changes
-
+  # runInsertTable(mat=audit, table="source_audit", db=db, get.id = FALSE)
+  # Query to join and make updates
+  updateQuery = paste0("UPDATE ", source," a INNER JOIN z_updated_df b ",
+                       "ON (a.source_hash = b.parent_hash) SET ",
+                       paste0("a.", names(live),  " = b.", names(live), collapse = ", ")
+                       )
+  # runUpdate(updateQuery=updateQuery, updated_df=live, db)
 }
 
 convert.audit.to.json <- function(in_dat){
