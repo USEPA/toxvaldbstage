55a504da076a0712d7c7e9469ea16a50b8bcf2b4
Taylor Wall <wall.jonathan@epa.gov>
2022-11-28 16:45:05 -0500

    Updates to code and renamed scripts according to output

--- R/QA/qa_function_file_name_check.R
+++ R/QA/qa_function_file_name_check.R
@@ -1,14 +1,20 @@
+# QA script to find functions not named the same as their source file, as
+# per he toxvaldb09 naming convention
+
 # ls the folder containing all R scripts
 file_list = list.files("R", full.names=TRUE)
 
-# remove deprecated folder
-file_list <- file_list[which(file_list != 'R/deprecated')]
+# remove deprecated and QA folder
+file_list <- file_list[!file_list %in% c('R/deprecated', 'R/QA')]
 
 # load all R functions in the toxvaldb repo by source()-ing each R file
 invisible(sapply(file_list, source,.GlobalEnv))
 
-# trim script names down to more easily match function names
-file_list_trimmed <- substring(file_list, 3, nchar(file_list) - 2)
+# get basename without file extension (functions supposed to be same name)
+file_list_trimmed <- tools::file_path_sans_ext(basename(file_list))
+
+#https://stackoverflow.com/questions/5103194/get-the-list-of-functions-loaded-in-rs-global-environment
+fn_list <- ls()[sapply(ls(), function(obj) "function"==class(eval(parse(text = obj)))[1])]
 
 # compare lists of functions and files
 extra_functions <- fn_list[which(!(fn_list %in% file_list_trimmed))]
@@ -16,3 +22,5 @@ extra_functions <- fn_list[which(!(fn_list %in% file_list_trimmed))]
 # list parents of unmatched functions (https://stackoverflow.com/a/32749240)
 parent_files <- sapply(extra_functions,
                        function(x){attr(attr(get(x), 'srcref'), 'srcfile')$filename})
+# See results
+View(parent_files)
