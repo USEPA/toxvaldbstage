b69b1562494f18484f871d5a999b19eb7dc21dc2
abrito <brito.anthony@epa.gov>
2023-02-02 09:32:07 -0500

    created new import file for efsa openfoodtox 2022 re-extraction, did transformations such as renaming columns, renaming/splitting columns based on old import_efsa_source file, and made a sample import file

---
+++ R/import_efsa_source_2022.R
@@ -0,0 +1,82 @@
+#--------------------------------------------------------------------------------------
+#' Import of EFSA OpenFoodTox 2022 source into toxval_source
+#'
+#' @param db The version of toxval_source into which the source is loaded.
+#' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
+#--------------------------------------------------------------------------------------
+import_generic_source <- function(db,chem.check.halt=F) {
+  printCurrentFunction(db)
+  source = "EFSA OpenFoodTox"
+  source_table = "source_efsa"
+  dir = paste0(toxval.config()$datapath,"efsa/efsa_files/")
+  file = paste0(dir,"efsa_openfoodtox_raw_2022.xlsx")
+  res0 = read.xlsx(file)
+  #####################################################################
+  cat("Do any non-generic steps to get the data ready \n")
+  #####################################################################
+  #
+  # the final file should have column names that include "name" and "casrn"
+  # additionally, the names in res need to match names in the source
+  # database table. You do not need to add any of the generic columns
+  # described in the SOP - they will get added in source_prep_and_load
+  #
+  
+  # Standardize the names
+  names(res0) <- names(res0) %>%
+    # Replace whitespace and periods with underscore
+    gsub("[[:space:]]|[.]", "_", .) %>%
+    stringr::str_squish() %>%
+    tolower()
+  
+  #res = source.specific.transformations(res0)
+  
+  res <- res0 %>%
+    # Renaming columns
+    dplyr::rename(record_url=url,
+                  toxval_type=endpoint,
+                  toxval_numeric=value,
+                  toxval_numeric_qualifier=qualifier_x,
+                  toxval_unit=doseunit,
+                  critical_effect=basis,
+                  study_type=testtype,
+                  study_duration_value=exp_duration_days,
+                  species_original=species,
+                  strain=strain,
+                  sex=sex,
+                  human_eco=study_category,
+                  year=publicationyear,
+                  title=title,
+                  record_source_type=doctype,
+                  casrn=com_casnumber,
+                  chemical=comparamname,
+                  source=owner,
+                  subsource=author) %>%
+    # Recoding/fixing entries in study_type
+    mutate(study_type=recode(study_type,
+                             "acute toxicity" = "acute",
+                             "chronic/long term toxicity" = "chronic/long-term",
+                             "reproduction toxicity" = "reproductive",
+                             "short-term toxicity" = "short-term",
+                             "study with volunteers" = "human")) %>%
+    mutate(human_eco=recode(human_eco,
+                            "Animal (non-target species) health" = "human health",
+                            "Animal (target species) health" = "human health",
+                            "Ecotox (soil compartment)" = "eco",
+                            "Ecotox (water compartment)" = "eco",
+                            "Human health" = "human health"))
+  
+  
+  # making source_url, source_file, and study_duration_units columns
+  res$source_url <- "https://zenodo.org/record/5076033#.Y9fEoXbMI2z" %>%
+    res$source_download <- "OpenFoodToxTX22784_2022.xlsx" %>%
+    res$study_duration_units <- "days"
+  
+  # splitting ROUTE into exposure_route and exposure_method columns
+  mutate(separate(res, route, c("exposure_route","exposure_method"), sep=": ", fill="right") -> res)
+  
+  #####################################################################
+  cat("Prep and load the data\n")
+  #####################################################################
+  source_prep_and_load(db,source=source,table=source_table,res=res,F,T,T)
+}
+
