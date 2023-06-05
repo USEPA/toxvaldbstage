9a14a3de88a1537fe258b3c91a9eb47ff327f126
Taylor Wall <wall.jonathan@epa.gov>
2023-06-05 12:13:39 -0400

    Update import hess documentation, function name, and minor revisions

--- R/import_source_hess.R
+++ R/import_source_hess.R
@@ -1,12 +1,12 @@
 #--------------------------------------------------------------------------------------
-#' @description A generic template for adding data to toxval_source for a new source
+#' @description Import of HESS data source
 #'
 #' @param db The version of toxval_source into which the source is loaded.
 #' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
 #' @param do.reset If TRUE, delete data from the database for this source before
 #' @param do.insert If TRUE, insert data into the database, default FALSE
-#' @title FUNCTION_TITLE
-#' @return OUTPUT_DESCRIPTION
+#' @title import_source_hess
+#' @return None. Data is processed into the database
 #' @details DETAILS
 #' @examples
 #' \dontrun{
@@ -22,7 +22,7 @@
 #' @importFrom readxl read_xlsx
 #' @importFrom stringr str_squish
 #--------------------------------------------------------------------------------------
-import_generic_source <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
+import_source_hess <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
   printCurrentFunction(db)
   source = "HESS"
   source_table = "source_hess"
@@ -40,22 +40,21 @@ import_generic_source <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.i
   # database table. You do not need to add any of the generic columns
   # described in the SOP - they will get added in source_prep_and_load
   #
-  
+
   # Standardize the names
   names(res0) <- names(res0) %>%
     stringr::str_squish() %>%
     # Replace whitespace and periods with underscore
     gsub("[[:space:]]|[.]", "_", .) %>%
     tolower()
-  
+
   #res = source.specific.transformations(res0)
-  
+
   res <- res0 %>%
     # Renaming columns
     dplyr::rename(casrn="cas_number",
                   name="chemical_name(s)",
                   ec_number="additional_ids",
-                  sources="identity",
                   study_type="endpointpath",
                   critical_effect="effect",
                   toxval_type="endpoint",
@@ -69,20 +68,21 @@ import_generic_source <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.i
     dplyr::mutate(
       # Removing "EC Number:" from ec_number field
       ec_number = gsub("EC Number:", "", ec_number),
-      # Removing "Sources:" from sources field
-      sources = gsub("Sources:", "", sources),
       # Removing "Human Health Hazards#" from study_type field
-      study_type = gsub("Human Health Hazards#", "", study_type),
+      # Lowercasing the study_type field values
+      study_type = gsub("Human Health Hazards#", "", study_type) %>%
+        tolower(),
       # Removing parenthesis around values in route_of_administration field
       route_of_administration = gsub("[()]", "", route_of_administration),
     ) %>%
     # Splitting route_of_administration field into exposure_route and exposure_method
     tidyr::separate(route_of_administration, c("exposure_route", "exposure_method"),
-                    sep=" ", fill="right", remove=TRUE)
-  
-  # Lowercasing the study_type field values
-  res$study_type <- tolower(res$study_type)
-    
+                    sep=" ", fill="right", extra = "merge", remove=FALSE)
+
+  # Check route_of_administration splitting
+  # View(res %>% select(route_of_administration, exposure_route, exposure_method) %>% distinct())
+  # TODO Check critical_effect encoded arrow symbol notation
+
   # Add version date. Can be converted to a mutate statement as needed
   res$source_version_date <- src_version_date
   #####################################################################
