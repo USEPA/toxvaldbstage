23a2bbe637758d144bd3222ee62443d8c212f0b1
Taylor Wall <wall.jonathan@epa.gov>
2023-07-18 15:21:03 -0400

    Generic import script for WHO IPCS

---
+++ R/import_source_who_ipcs.R
@@ -0,0 +1,69 @@
+#--------------------------------------------------------------------------------------
+#' @description Import of WHO IPCS data
+#'
+#' @param db The version of toxval_source into which the source is loaded.
+#' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
+#' @param do.reset If TRUE, delete data from the database for this source before
+#' @param do.insert If TRUE, insert data into the database, default FALSE
+#' @title import_who_ipcs
+#' @return None. Data is processed into the toxval_source database
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[readxl]{read_excel}}
+#'  \code{\link[stringr]{str_trim}}
+#' @rdname import_generic_source
+#' @export
+#' @importFrom readxl read_xlsx
+#' @importFrom stringr str_squish
+#--------------------------------------------------------------------------------------
+import_who_ipcs <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
+  printCurrentFunction(db)
+  source = "WHO IPCS"
+  source_table = "source_who_ipcs"
+  # Date provided by the source or the date the data was extracted
+  src_version_date = as.Date("2019-01-01")
+  dir = paste0(toxval.config()$datapath,"who_ipcs/who_ipcs_files/")
+  file = paste0(dir,"name of the source file.xlsx")
+  res0 = readxl::read_xlsx(file)
+  #####################################################################
+  cat("Do any non-generic steps to get the data ready \n")
+  #####################################################################
+  #
+  # the final file should have column names that include "name" and "casrn"
+  # additionally, the names in res need to match names in the source
+  # database table. You do not need to add any of the generic columns
+  # described in the SOP - they will get added in source_prep_and_load
+  #
+
+  # Standardize the names
+  names(res0) <- names(res0) %>%
+    stringr::str_squish() %>%
+    # Replace whitespace and periods with underscore
+    gsub("[[:space:]]|[.]", "_", .) %>%
+    tolower()
+
+  res = source.specific.transformations(res0)
+
+  # Add version date. Can be converted to a mutate statement as needed
+  res$source_version_date <- src_version_date
+  #####################################################################
+  cat("Prep and load the data\n")
+  #####################################################################
+  source_prep_and_load(db=db,
+                       source=source,
+                       table=source_table,
+                       res=res,
+                       do.reset=do.reset,
+                       do.insert=do.insert,
+                       chem.check.halt=chem.check.halt)
+}
+
+
+
+
