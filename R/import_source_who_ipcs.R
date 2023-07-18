5262c5b14cdf73ac0b0ad6f7795fddb37eda2304
Taylor Wall <wall.jonathan@epa.gov>
2023-07-18 18:09:15 -0400

    Draft updated WHO IPCS import

--- R/import_source_who_ipcs.R
+++ R/import_source_who_ipcs.R
@@ -29,8 +29,96 @@ import_who_ipcs <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=
   # Date provided by the source or the date the data was extracted
   src_version_date = as.Date("2019-01-01")
   dir = paste0(toxval.config()$datapath,"who_ipcs/who_ipcs_files/")
-  file = paste0(dir,"name of the source file.xlsx")
-  res0 = readxl::read_xlsx(file)
+  file = paste0(dir,"who_ipcs_2019_raw_combined.xlsx")
+  res0_sheets <- readxl::excel_sheets(file) %>%
+    .[grepl("^Table", .)]
+  table_names <- readxl::read_xlsx(file, sheet="table_names")
+  res0_caption_key <- readxl::read_xlsx(file, sheet="caption_key")
+  res0 <- lapply(res0_sheets, function(s){
+    tmp <- readxl::read_xlsx(file, sheet=s) %>%
+      dplyr::mutate(table_name = table_names$name[table_names$table == s])
+
+    # Fix field name issues
+    names(tmp) <- names(tmp) %>%
+      stringr::str_squish() %>%
+      gsub("LD_\\{50\\} mg/kg|LD_\\{50\\} mg/ kg", "LD50_mg/kg", .)
+
+    return(tmp)
+
+  }) %>%
+    dplyr::bind_rows()
+
+  res = res0 %>%
+    # Rename toxval names
+    dplyr::rename(name = `Common name`,
+                  cas = `CAS no`,
+                  toxval_numeric = `LD50_mg/kg`) %>%
+    dplyr::mutate(subsource = "Pesticide Classification 2019",
+                  source_url = "https://www.who.int/publications/i/item/9789240005662",
+                  toxval_type = "LD50",
+                  toxval_units = "mg/kg",
+                  risk_assessment_class = "TBD",
+                  toxval_numeric_qualifier = "=",
+                  exposure_route = "TBD",
+                  name = name %>%
+                    # Remove [ISO] from chemical name
+                    gsub("\\[ISO\\]", "", .)) %>%
+    # TODO insert toxval_numeric_qualitifer from toxval_numeric (e.g., >, <)
+    # toxval_numeric_qualifier (=, >, <, >=)
+    # replace key abbreviated values with text values using dictionary
+    dplyr::mutate(dplyr::across(c("Chem type", "Phys state", "Main use"),
+                                            # Dictionary for Chem type
+                  .fns = ~ dplyr::case_when(`.` == "AS" ~ "Arsenic compound",
+                                            `.` == "BP" ~ "Bipyridylium derivative",
+                                            `.` == "C" ~ "Carbamate",
+                                            `.` == "CO" ~ "Coumarin derivative",
+                                            `.` == "CU" ~ "Copper compound",
+                                            `.` == "HG" ~ "Mercury compound",
+                                            `.` == "NP" ~ "Nitrophenol derivative",
+                                            `.` == "OC" ~ "Organochlorine compound",
+                                            `.` == "OP" ~ "Organophosphorus compound",
+                                            `.` == "OT" ~ "Organotin compound",
+                                            `.` == "PAA" ~ "Phenoxyacetic acid derivative",
+                                            `.` == "PZ" ~ "Pyrazole",
+                                            `.` == "PY" ~ "Pyrethroid",
+                                            `.` == "T" ~ "Triazine derivative",
+                                            `.` == "TC" ~ "Thiocarbamate",
+                                            # Physical state dictionary
+                                            `.` == "S" ~ "Active ingredient is Solid, including waxes",
+                                            `.` == "L" ~ "Active ingredient is Liquid, including solids with a melting point below 50 Celsius",
+                                            `.` == "Oil" ~ "Active ingredient is oily liquid",
+                                            `.` == "oil" ~ "Active ingredient is oily liquid",
+                                            # Main Use Dictionary
+                                            `.` == "AC" ~ "acaricide",
+                                            `.` == "AP" ~ "aphicide",
+                                            `.` == "B" ~ "bacteriostat (soil)",
+                                            `.` == "FM" ~ "fumigant",
+                                            `.` == "F" ~ "fungicide, other than for seed treatment",
+                                            `.` == "FST" ~ "fungicide, for seed treatment",
+                                            `.` == "H" ~ "herbicide",
+                                            `.` == "I" ~ "insecticide",
+                                            `.` == "IGR" ~ "insect growth regulator",
+                                            `.` == "Ix" ~ "ixodicide (for tick control)",
+                                            `.` == "L" ~ "larvicide",
+                                            `.` == "M" ~ "molluscicide",
+                                            `.` == "MT" ~ "miticide",
+                                            `.` == "N" ~ "nematocide",
+                                            `.` == "O" ~ "other use for plant pathogens",
+                                            `.` == "PGR" ~ "plant growth regulator",
+                                            `.` == "R" ~ "rodenticide",
+                                            `.` == "RP" ~ "repellant",
+                                            `.` == "-S" ~ "applied to soil: not used with herbicides or plant growth regulators",
+                                            `.` == "SY" ~ "synergist",
+                                            TRUE ~ `.`
+                                            )
+                  )
+                  )
+
+
+
+  # Fill qualifier NA with "="
+  res$toxval_numeric_qualifier[is.na(res$toxval_numeric_qualifier)] = "="
+
   #####################################################################
   cat("Do any non-generic steps to get the data ready \n")
   #####################################################################
