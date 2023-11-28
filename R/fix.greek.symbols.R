97687c25a5cac0345de52163bba01096699b4b5f
Winter <Winter.Samuel@epa.gov>
2023-11-28 17:18:38 -0500

    Handle symbols and remove population hardcoding.

--- R/fix.greek.symbols.R
+++ R/fix.greek.symbols.R
@@ -20,36 +20,51 @@ fix.greek.symbols <- function(df) {
     cat("fix.greek.symbols input must be character vector...\n")
     return()
   }
-
+  
   # Generate unicode lists
   # TODO Add more cases to handle
   # USE stringi::stri_escape_unicode("") to assist with identifying new encoding
   # https://en.wikipedia.org/wiki/Greek_script_in_Unicode
   alpha = c("<U+1D6C2>", "<U+1D6FC>", "<U+1D736>", "<U+1D770>", "<U+1D7AA>",
             "\u03B1", "\u1D6C2", "\u1D6FC", "\u1D736", "\u1D770", "\u1D7AA", "<U+03B1>")
-
+  
   beta = c("<U+03B2>", "<U+03D0>", "<U+1D6C3>", "<U+1D6FD>", "<U+1D737>", "<U+1D771>", "<U+1D7AB>",
            "<U+0392>", "<U+1D6A9>", "<U+1D6E3>", "<U+1D71D>", "<U+1D757>", "<U+1D791>", "<U+1D5D>",
            "<U+1D66>", "<U+00df?",
            "\u03B2", "\u03D0", "\u1D6C3", "\u1D6FD", "\u1D737", "\u1D771", "\u1D7AB",
            "\u0392", "\u1D6A9", "\u1D6E3", "\u1D71D", "\u1D757", "\u1D791", "\u1D5D",
            "\u1D66", "\u00df")
-
+  
   gamma = c("<U+03B3>", "<U+213D>", "<U+1D6C4>", "<U+1D6FE>", "<U+1D738>", "<U+1D772>", "<U+1D7AC>",
             "<U+0393>", "<U+213E>", "<U+1D6AA>", "<U+1D6E4>", "<U+1D71E>", "<U+1D758>", "<U+1D792>",
             "<U+1D5E>", "<U+1D67>", "<U+1D26>", "<U+0263>",
             "\u03B3", "\u213D", "\u1D6C4", "\u1D6FE", "\u1D738", "\u1D772", "\u1D7AC",
             "\u0393", "\u213E", "\u1D6AA", "\u1D6E4", "\u1D71E", "\u1D758", "\u1D792",
             "\u1D5E", "\u1D67", "\u1D26", "\u0263")
-
+  
   mu = c("<U+03BC>", "<U+00B5>", "<U+1D6CD>", "<U+1D707>", "<U+1D741>", "<U+1D77B>", "<U+1D7B5>",
          "\u03BC", "\u00B5", "\u1D6CD", "\u1D707", "\u1D741", "\u1D77B", "\u1D7B5")
-
+  
+  epsilon = c("<U+03B5", "\u03b5")
+  
+  lambda = c("<U+03BB>", "\u03bb")
+  
+  kappa = c("<U+03BA>", "\u03ba")
+  
+  omega = c("<U+03C9", "\u03c9")
+  
+  eta = c("<U+03B7>", "\u03b7")
+  
   df %>%
     # Replacements from: https://www.rapidtables.com/math/symbols/greek_alphabet.html
     gsub(paste0(alpha, collapse="|"), "a", .) %>%
     gsub(paste0(beta, collapse="|"), "b", .) %>%
     gsub(paste0(gamma, collapse="|"), "g", .) %>%
+    gsub(paste0(epsilon, collapse="|"), "e", .) %>%
+    gsub(paste0(lambda, collapse="|"), "l", .) %>%
+    gsub(paste0(kappa, collapse="|"), "k", .) %>%
+    gsub(paste0(omega, collapse="|"), "o", .) %>%
+    gsub(paste0(eta, collapse="|"), "h", .) %>%
     # For "micro" units like micrograms (ug)
     gsub(paste0(mu, collapse="|"), "u", .) %>%
     # gsub(paste0(rho, collapse="|"), "r", ., fixed=TRUE) %>%
--- R/import_source_gestis_dnel.R
+++ R/import_source_gestis_dnel.R
@@ -86,14 +86,53 @@ import_source_gestis_dnel <- function(db,chem.check.halt=FALSE, do.reset=FALSE, 
       toxval_units = "mg/mg3",
       study_type = "long-term occupational",
       species = "human",
-      population = "workers",
       exposure_route = "inhalation",
 
       # Add version date
       source_version_date = src_version_date,
 
-      # Fix Greek symbols in name field
-      Name = fix.greek.symbols(Name),
+      # Fix symbols in name field
+      Name = Name %>%
+        # Fix Greek symbols
+        fix.greek.symbols() %>%
+
+        # Remove trademark symbols
+        gsub("\u00ae|<U+00ae>", "", name) %>%
+
+        # Fix whitespace
+        gsub("[\r\n][\r\n]", " ", .) %>%
+        gsub("\u00a0|<U+00A0>", " ", .) %>%
+
+        # Fix quotations and apostrophes
+        gsub("\u201c|<U+201C>|\u201d|<U+201D>", '"', .) %>%
+        gsub("\u2018|<U+2018>|\u0092|<U+0092>|\u2019|<U+2019>", "'", .) %>%
+
+        # Fix superscript/subscript
+        gsub("\u00b3|<U+00B3>", "3", .) %>%
+        gsub("\u00b9|<U+00B9>", "1", .) %>%
+        gsub("\u2070|<U+2070>", "0", .) %>%
+        gsub("\u00b2|<U+00B2>", "2", .) %>%
+        gsub("\u2079|<U+2079>", "9", .) %>%
+        gsub("\u2078|<U+2078>", "8", .) %>%
+        gsub("\u2074|<U+2074>", "4", .) %>%
+        gsub("\u2077|<U+2077>", "7", .) %>%
+        gsub("\u2076|<U+2076>", "6", .) %>%
+
+        # Fix general punctuation
+        gsub("\u00b4|<U+00B4>", "'", .) %>%
+        gsub("\u2013|<U+2013>", "-", .) %>%
+        gsub("\u00bf|<U+00BF>", "?", .) %>%
+
+        # Fix math symbols
+        gsub("\u2265|<U+2265>", " greater than or equal to ", .) %>%
+        gsub("\u00b1|<U+00B1>", "+/-", .) %>%
+        gsub("\u00b0|<U+00B0>", "", .) %>%
+        gsub("\u00b0|<U+00B0>", "", .) %>%
+        gsub("\u2032|<U+2032>", "", .) %>%
+        gsub("\u00b7|<U+00B7>", "*", .) %>%
+
+        # Remove excess whitespace
+        stringr::str_squish(),
 
       # Replace "link" column with hyperlink URL (ZVG append must be length of 6)
       Link = ifelse(!is.na(ZVG) & nchar(ZVG) == 6, paste0("https://gestis-database.dguv.de/data?name=", ZVG),
@@ -119,7 +158,6 @@ import_source_gestis_dnel <- function(db,chem.check.halt=FALSE, do.reset=FALSE, 
 
   # Fill blank hashing cols
   res[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res)]] <- "-"
-
   #####################################################################
   cat("Prep and load the data\n")
   #####################################################################
