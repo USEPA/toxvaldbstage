ee62794620aa7abbc6f06fd98afbfbfaeb5b5c46
Taylor Wall <wall.jonathan@epa.gov>
2023-12-07 16:10:39 -0500

    Moved IRIS summary add to very end, only appending IRIS Summary with some renaming.

--- R/import_source_iris.R
+++ R/import_source_iris.R
@@ -385,40 +385,11 @@ import_source_iris <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.inse
     gsub("[[:space:]]|[.]", "_", .) %>%
     tolower()
 
-  # Rename before potential merge to avoid error
-  res0 <- res0 %>%
-    dplyr::rename(study_type = assessment_type)
-
-  # Set defaults for new fields
-  res0$document_type = 'IRIS Export'
-  res0$key_finding = 'No'
-
-  # Set key_findings, to yes if not in subset of toxval_type
-  not_key_finding = c("RfD", "RfC", "Cancer Slope", "Unit Risk Factor")
-  key_finding_type = subset(res0$toxval_type, !(res0$toxval_type %in% not_key_finding))
-  res0$key_finding[res0$toxval_type %in% key_finding_type] = "Yes"
-
-  # Add summary data to df before prep and load
-  if(do.summary_data){
-    # Merge to add chemical rev history fields to summary data
-    chem_rev <- iris_data$Chemical_Rev_History.xlsx[, !names(iris_data$Chemical_Rev_History.xlsx) %in% "URL"]
-    res1 <- merge(iris_data$test_source_iris_summary_curation_2023125.xlsx,
-                  chem_rev, by.x = "name",
-                  by.y = "CHEMICAL NAME", all.x = TRUE)
-    res1 <- res1 %>%
-      dplyr::rename(casrn = `CASRN`,
-                    iris_chemical_id = `CHEMICAL ID`)
-    res1$source_version_date <- src_version_date
-    res1$document_type = 'IRIS Summary'
-    res1$key_finding = 'No'
-    res0 <- merge(res0, res1, all=TRUE)
-  }
-
   # Add study_type, study_duration_class, and exposure_route, fix critical effect
   res0 <- res0 %>%
     dplyr::rename(exposure_route=route,
-                  study_duration_class=risk_assessment_duration,
-                  lifestage=age_original) %>%
+                  study_type = assessment_type,
+                  study_duration_class=risk_assessment_duration) %>%
     tidyr::unite(col="study_type", study_type, principal_study, sep=" - ", remove = FALSE) %>%
     # Combine endpoint and critical_effect
     tidyr::unite(col="critical_effect", endpoint, critical_effect, sep=": ", remove = FALSE) %>%
@@ -436,8 +407,44 @@ import_source_iris <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.inse
     dplyr::left_join(iris_year_map,
                      by="iris_chemical_id")
 
+################################################################################
+### Append IRIS Summary information
+################################################################################
+
+  # Set defaults for new fields
+  res0$document_type = 'IRIS Export'
+  # Set key_findings, to yes if not in subset of toxval_type
+  not_key_finding = c("RfD", "RfC", "Cancer Slope", "Unit Risk Factor", "Inhalation Unit Risk", "Oral Slope Factor")
+  res0$key_finding = ifelse(res0$toxval_type %in% not_key_finding, 'No', 'Yes')
+
+  # Add summary data to df before prep and load
+  if(do.summary_data){
+    # Import manually curated IRIS Summary information
+    res1 <- iris_data$test_source_iris_summary_curation_2023125.xlsx %>%
+      dplyr::mutate(
+        source_version_date = src_version_date,
+        document_type = 'IRIS Summary',
+        key_finding = 'No',
+        iris_chemical_id = url %>%
+          sub('.*=', '', .) %>%
+          as.numeric()) %>%
+      # Remove IRIS Export fields
+      # dplyr::select(-principal_study, -document_type, -endpoint) %>%
+      dplyr::rename(
+        long_ref=full_reference,
+        exposure_route = route,
+        species = species_original,
+        sex = sex_original,
+        age = age_original
+      )
+    res = res0 %>%
+      dplyr::bind_rows(res1)
+  } else {
+    res = res0
+  }
+
   # Fill blank hashing cols
-  res0[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res0)]] <- "-"
+  res[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res)]] <- "-"
 
   #####################################################################
   cat("Prep and load the data\n")
@@ -445,7 +452,7 @@ import_source_iris <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.inse
   source_prep_and_load(db=db,
                        source=source,
                        table=source_table,
-                       res=res0,
+                       res=res,
                        do.reset=do.reset,
                        do.insert=do.insert,
                        chem.check.halt=chem.check.halt,
