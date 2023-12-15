614e403cddbf2fa8976d5d290c6ce0cd8470a7ad
Taylor Wall <wall.jonathan@epa.gov>
2023-12-15 15:25:38 -0500

    Simplified logic with tidyr::pivot and mutate with gsub to get toxval_type, subtype, numeric, and units. Added hardcoded toxval fields.

--- R/import_source_usgs_hbsl.R
+++ R/import_source_usgs_hbsl.R
@@ -30,7 +30,7 @@ import_source_usgs_hbsl <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do
   src_version_date = as.Date("2018-05-31")
   dir = paste0(toxval.config()$datapath,"usgs_hbsl/usgs_hbsl_files/")
   file = paste0(dir,"HBSL-data_20180531.xlsx")
-  res1 = readxl::read_xlsx(file, skip = 1)
+  res0 = readxl::read_xlsx(file, skip = 1)
   #####################################################################
   cat("Do any non-generic steps to get the data ready \n")
   #####################################################################
@@ -40,107 +40,73 @@ import_source_usgs_hbsl <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do
   # database table. You do not need to add any of the generic columns
   # described in the SOP - they will get added in source_prep_and_load
   #
-  names(res1) <- sub("\\s*\\(.*\\)","", names(res1))
-  res0 <- res1 %>%
+  # names(res1) <- sub("\\s*\\(.*\\)","", names(res1))
+  res0 <- res0 %>%
     dplyr::mutate(name = `Chemical Name` %>%
-                    # Replace Prime symbol
-                    gsub("\u00b4|<U+00B4>|\u2018|<U+2018>|\u0092|<U+0092>|\u2019|<U+2019>|\u2032", "'", .) %>%
-                    fix.greek.symbols(),
-                  # Fix several cases where casrn gets reformatted as mm/dd/yyyy
+                    fix.replace.unicode(),
                   casrn = ifelse(grepl("/", `CAS Registry Number`),
-                                   sapply(strsplit(as.character(`CAS Registry Number`), "/"),
-                                                   function(x){
-                                                     year <- x[3]
-                                                     month <- sprintf("%02d", as.integer(x[1]))
-                                                     day <- x[2]
-                                                     paste(year, month, day, sep = '-')
-                                                   }),
+                                 sapply(strsplit(as.character(`CAS Registry Number`), "/"),
+                                        function(x){
+                                          year <- x[3]
+                                          month <- sprintf("%02d", as.integer(x[1]))
+                                          day <- x[2]
+                                          paste(year, month, day, sep = '-')
+                                        }),
                                  `CAS Registry Number`
-                                )
+                  )
                   )
 
-  # Split rows where multiple columns have numeric values before derivation below
-  res0 <- res0 %>%
-    dplyr::bind_rows(
-      res0 %>%
-        dplyr::filter(!is.na(`Noncancer HBSL`) & !is.na(`Cancer HBSL`)) %>%
-        dplyr::mutate(
-          `Cancer HBSL` = NA_character_
-        ),
-      res0 %>%
-        dplyr::filter(!is.na(`Noncancer HBSL`) & !is.na(`Cancer HBSL`)) %>%
-        dplyr::mutate(
-          `Noncancer HBSL` = NA_real_
-        ),
-      res0 %>%
-        dplyr::filter(!is.na(`Chronic Noncancer HHBP`) & !is.na(`Carcinogenic HHBP`)) %>%
-        dplyr::mutate(
-          `Chronic Noncancer HHBP` = NA_real_
-        ),
-      res0 %>%
-        dplyr::filter(!is.na(`Chronic Noncancer HHBP`) & !is.na(`Carcinogenic HHBP`)) %>%
-        dplyr::mutate(
-          `Carcinogenic HHBP` = NA_character_
-        )
-    ) %>%
-    select(names(res0))
+  # Pivot fields
+  res = res0 %>%
+    tidyr::pivot_longer(cols=c(`MCL (micrograms/L)`,
+                               `Chronic Noncancer HHBP (micrograms/L)`,
+                               `Carcinogenic HHBP (micrograms/L)`, `Noncancer HBSL (micrograms/L)`,
+                               `Cancer HBSL (micrograms/L)`),
+                        names_to = "toxval_type",
+                        values_to = "toxval_numeric",
+                        values_transform = as.character,
+                        # Remove NA values
+                        values_drop_na = TRUE
+                        ) %>%
+    tidyr::separate(col="toxval_type",
+                    into = c("toxval_type", "toxval_units"), sep = "\\(") %>%
+    dplyr::mutate(toxval_subtype = toxval_type %>%
+                    gsub("HBSL|HHBP|MCL", "", .) %>%
+                    gsub("Carcinogenic", "Cancer", .) %>%
+                    stringr::str_squish() %>%
+                    dplyr::na_if(""),
+                  toxval_type = toxval_type %>%
+                    gsub("Noncancer|Chronic|Cancer|Carcinogenic", "", .) %>%
+                    stringr::str_squish(),
+                  toxval_units = toxval_units %>%
+                    # Remove training parentheses
+                    gsub("\\)", "", .),
+                  source = "USGS HBSL",
+                  subsource = "Water Quality Data",
+                  source_url = "https://water.usgs.gov/water-resources/hbsl",
+                  risk_assessment_class = "chronic",
+                  exposure_route = "oral",
+                  # Add version date. Can be converted to a mutate statement as needed
+                  source_version_date = src_version_date,
+                  long_ref = paste0("Norman, J.E., Toccalino, P.L., Morman, S.A., 2018, ",
+                                    "Health-Based Screening Levels for evaluating water-quality data (2d ed.). ",
+                                    "U.S. Geological Survey web page, accessible at ",
+                                    "https://water.usgs.gov/water-resources/hbsl/, doi:10.5066/F71C1TWP."))
 
-  # Remove original, non-split rows
-  res0 <- res0 %>%
-    dplyr::filter(!((!is.na(`Noncancer HBSL`) & !is.na(`Cancer HBSL`)) |
-             (!is.na(`Chronic Noncancer HHBP`) & !is.na(`Carcinogenic HHBP`))))
-
-  # Handle hyphenated numeric values
-  res0 <- res0 %>%
-    dplyr::mutate(
-      final_cancer_hbsl = as.numeric(str_extract(`Cancer HBSL`, "(?<=-)[0-9.]+")),
-      final_carcinogenic_hhbp = as.numeric(str_extract(`Carcinogenic HHBP`, "(?<=-)[0-9.]+"))
-    )
-
-  # Set toxval fields based on which column the toxval_numeric is coming from
-  res0 <- res0 %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(
-      toxval_type = case_when(
-        !is.na(`MCL`) ~ "MCL",
-        !is.na(`Chronic Noncancer HHBP`) ~ "HHBP",
-        !is.na(`Carcinogenic HHBP`) ~ "HHBP carcinogenicity",
-        !is.na(`Noncancer HBSL`) ~ "HBSL",
-        !is.na(final_cancer_hbsl) ~ "HBSL",
-        TRUE ~ NA_character_
-      ),
-      toxval_subtype = case_when(
-        !is.na(`MCL`) ~ "-",
-        !is.na(`Chronic Noncancer HHBP`) ~ "Chronic, non_cancer",
-        !is.na(`Carcinogenic HHBP`) ~ "Cancer",
-        !is.na(`Noncancer HBSL`) ~ "Noncancer",
-        !is.na(final_cancer_hbsl) ~ "Cancer",
-        TRUE ~ NA_character_
-      ),
-      toxval_numeric = case_when(
-        !is.na(`MCL`) ~ `MCL`,
-        !is.na(`Chronic Noncancer HHBP`) ~ `Chronic Noncancer HHBP`,
-        !is.na(final_carcinogenic_hhbp) ~ final_carcinogenic_hhbp,
-        !is.na(`Noncancer HBSL`) ~ `Noncancer HBSL`,
-        !is.na(final_cancer_hbsl) ~ final_cancer_hbsl,
-        TRUE ~ NA_real_
-      )) %>%
-    dplyr::mutate(
-      toxval_units = ifelse(!is.na(toxval_numeric),"microgram/L", NA_character_)
-    )
 
   # Standardize the names
-  names(res0) <- names(res0) %>%
+  names(res) <- names(res) %>%
     stringr::str_squish() %>%
     # Replace whitespace and periods with underscore
     gsub("[[:space:]]|[.]", "_", .) %>%
     tolower()
 
   # Fill blank hashing cols
-  res0[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res0)]] <- "-"
+  res[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res)]] <- "-"
 
-  # Add version date. Can be converted to a mutate statement as needed
-  res0$source_version_date <- src_version_date
+  # Reorder hashing cols to end of dataframe
+  res = res %>%
+    dplyr::select(dplyr::everything(), dplyr::all_of(toxval.config()$hashing_cols))
   #####################################################################
   cat("Prep and load the data\n")
   #####################################################################
