ab6342ab6f44e80c63025e732a54e3cba9919bdc
Winter <Winter.Samuel@epa.gov>
2024-01-11 10:14:54 -0500

    Updated import script to use manually curated data

--- R/import_source_who_jecfa_tox_studies.R
+++ R/import_source_who_jecfa_tox_studies.R
@@ -1,12 +1,11 @@
 #--------------------------------------------------------------------------------------
+#' @title import_source_who_jecfa_tox_studies
 #' @description Import of WHO JECFA Tox Studies data
-#'
 #' @param db The version of toxval_source into which the source is loaded.
 #' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
 #' @param do.reset If TRUE, delete data from the database for this source before
-#' @param do.insert If TRUE, insert data into the database, default FALSE
-#' @title FUNCTION_TITLE
-#' @return OUTPUT_DESCRIPTION
+#' @param do.insert If TRUE, insert data into the database, default FALS
+#' @return None; data is pushed to toxval_source
 #' @details DETAILS
 #' @examples
 #' \dontrun{
@@ -16,10 +15,14 @@
 #' }
 #' @seealso
 #'  \code{\link[readxl]{read_excel}}
+#'  \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{case_when}}, \code{\link[dplyr]{filter}}
+#'  \code{\link[tidyr]{separate_rows}}
 #'  \code{\link[stringr]{str_trim}}
-#' @rdname import_generic_source
+#' @rdname import_source_who_jecfa_tox_studies
 #' @export
 #' @importFrom readxl read_xlsx
+#' @importFrom dplyr mutate case_when filter
+#' @importFrom tidyr separate_rows
 #' @importFrom stringr str_squish
 #--------------------------------------------------------------------------------------
 import_source_who_jecfa_tox_studies <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
@@ -29,82 +32,54 @@ import_source_who_jecfa_tox_studies <- function(db,chem.check.halt=FALSE, do.res
   # Date provided by the source or the date the data was extracted
   src_version_date = as.Date("2022-11-01")
   dir = paste0(toxval.config()$datapath,"who_jecfa_tox_studies/who_jecfa_tox_studies_files/")
-  file = paste0(dir,"source_who_jecfa_raw_toxicological_data_20231101.xlsx")
+  file = paste0(dir,"who_jecfa_toxicological_data_manual.xlsx")
   res0 = readxl::read_xlsx(file)
   #####################################################################
   cat("Do any non-generic steps to get the data ready \n")
   #####################################################################
-  #
-  # the final file should have column names that include "name" and "casrn"
-  # additionally, the names in res need to match names in the source
-  # database table. You do not need to add any of the generic columns
-  # described in the SOP - they will get added in source_prep_and_load
-  #
   res = res0 %>%
     # Copy toxval fields from originals
-    dplyr::mutate(name = `Webpage Name` %>%
-                    # Replace trademark/copyright symbol
-                    gsub("\u00ae|<U+00ae>|\u00a9|\u2122", "", .) %>%
-                    fix.greek.symbols(),
-                  casrn = `CAS number` %>%
-                    # Remove parenthetics
-                    gsub("\\s*\\([^\\)]+\\)","", .),
-                  critical_effect = `Effect`,
-                  species = `Animal Specie`,
-                  year = `Evaluation year`) %>%
-    tidyr::separate_rows(casrn, sep=";") %>%
-    dplyr::mutate(casrn = casrn %>%
-                    stringr::str_squish())
+    dplyr::mutate(
+      name = fix.replace.unicode(name) %>% toupper(),
+      casrn = gsub("\\s*\\([^\\)]+\\)","", casrn),
+      toxval_units = fix.replace.unicode(toxval_units),
 
-  res = res %>%
-    pivot_longer(cols = c(NOAEL, NOEL, LOEL, PMTDI, LOAEL, PTWI, PTMI, PTDI),
-                 names_to= "toxval_type",
-                 values_to = "value") %>%
-    tidyr::separate_rows(value, sep = ';') %>%
-    dplyr::mutate(
-      value = fix.greek.symbols(value)
-    ) %>%
-    # NOAEL special case of "Rats only:  0.67 mg/kg bw/d (males) and 1.88 mg/kg bw/d (females)"
-    # Handle before parenthetic and other splitting. Cannot be on "and" alone
-    tidyr::separate_rows(value, sep = "\\) and ") %>%
-    dplyr::mutate(
-      cleaned_value = value %>%
-        stringr::str_squish() %>%
-        # Replace None/none with NA
-        gsub("^none.*", NA, ., ignore.case = TRUE) %>%
-        gsub("\\(.*?\\)","",.) %>%
-        gsub("–", "-", .) %>%
-        gsub("\\s*-\\s*", "-", .) %>%
-        # Remove extraneous strings
-        gsub(", rats and pigs| in the diet, acute NOEL|2.5 ug/p,", "", .) %>%
-        stringr::str_squish()) %>%
-
-    dplyr::mutate(
-      toxval_numeric = cleaned_value %>%
-        stringr::str_extract(., "\\d+\\.?\\d*-?\\d*\\.?\\d*"),
-      # Fixes a few cases of the toxval_numeric occurring twice in the string, causing issues with toxval_units
-      toxval_units = stringr::str_match(cleaned_value, paste0(toxval_numeric, "(.*)"))[,2],
-      toxval_units = stringr::str_trim(toxval_units),
-      toxval_units = if_else(is.na(toxval_numeric), NA_character_, toxval_units),
-      toxval_numeric_qualifier = dplyr::case_when(
-        grepl(">", cleaned_value) ~ ">",
-        grepl("<", cleaned_value) ~ "<",
-        grepl("=", cleaned_value) ~ "=",
-        grepl("~", cleaned_value) ~ "~",
-        TRUE ~ "-"
+      # Replace non-number toxval_numeric values with blank entry (not NA to preserve char col)
+      toxval_numeric = dplyr::case_when(
+        grepl("[a-zA-Z]", toxval_numeric) ~ "-",
+        TRUE ~ toxval_numeric
       ),
-      # Extract sex
-      sex = stringr::str_extract(value, "\\(males|\\(females")
-    ) %>%
-    dplyr::mutate(
-      sex = if_else(!is.na(sex), stringr::str_extract(sex, "(males|females)"), "-"),
-    ) %>%
-    dplyr::filter(!is.na(toxval_numeric)) %>%
-    dplyr::select(-cleaned_value, -value)
 
-  # Check transformations to ensure they worked as expected
-  # res %>% select(toxval_type, value, cleaned_value) %>% distinct() %>% View()
-  # res %>% select(toxval_type, value, cleaned_value, toxval_numeric, toxval_units) %>% distinct() %>% View()
+      # Translate sex into M/F format
+      sex = dplyr::case_when(
+        sex == "male" ~ "M",
+        sex == "female" ~ "F",
+        sex == "male; female" ~ "M/F",
+        TRUE ~ sex
+      ),
+
+      # Set "none"-type critical_effect to NA
+      critical_effect = dplyr::case_when(
+        critical_effect %in% c("None",
+                               "None reported",
+                               "not specified",
+                               "No significant adverse effects") ~ NA,
+        TRUE ~ critical_effect
+      )
+    ) %>%
+
+    # Drop rows without toxval_numeric
+    dplyr::filter(toxval_numeric != "-") %>%
+
+    # Remove entries with blank toxval_type
+    dplyr::filter(toxval_type != "-") %>%
+
+    # Handle casrn
+    tidyr::separate_rows(casrn, sep=";") %>%
+    dplyr::mutate(
+      casrn = casrn %>%
+        stringr::str_squish(),
+    )
 
   # Standardize the names
   names(res) <- names(res) %>%
