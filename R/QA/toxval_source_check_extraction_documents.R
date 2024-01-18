f2c9c6fcf82a6797b905090333760832f9d65573
Winter <Winter.Samuel@epa.gov>
2024-01-18 10:37:13 -0500

    State of script before adjusting to use source_hash

--- R/QA/toxval_source_check_extraction_documents.R
+++ R/QA/toxval_source_check_extraction_documents.R
@@ -1,24 +1,25 @@
 #-------------------------------------------------------------------------------------
 #' @title toxval_source_check_extraction_documents
 #' @description Check which sources do not have "extraction document" associations
-#' wit their records (by source hash)
+#' in their records (by source hash) and get list of docs with null document_type
 #' @param db The version of toxval source database to use.
 #' @param write_output Whether to write output to Excel (FALSE by default)
-#' @return List of sources without extraction document associations. Export XLSX file
-#' is also produced.
+#' @return List of sources without extraction document associations and document information
+#' for docs without document_type. Export XLSX files produced if specified.
 #' @import RMySQL dplyr readxl magrittr
 #--------------------------------------------------------------------------------------
 toxval_source_check_extraction_documents <- function(db, write_output=FALSE){
 
   # Query to check document type counts for each source
-  doc_query = paste0("SELECT DISTINCT a.source_table, b.document_type, count(*) AS n ",
-                     "FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
-                     "WHERE a.source_table IN ",
-                     "(SELECT source_table FROM chemical_source_index WHERE source_status='active') ",
-                     "GROUP BY a.source_table, b.document_type")
+  doc_type_query = paste0("SELECT DISTINCT a.source_table, b.document_type, count(*) AS n ",
+                          "FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
+                          "WHERE a.source_table IN ",
+                          "(SELECT source_table FROM chemical_source_index ",
+                          "WHERE source_status='active') ",
+                          "GROUP BY a.source_table, b.document_type")
 
   # Get list of all active tables with extraction document associations
-  active_extraction = runQuery(doc_query, db) %>%
+  active_extraction = runQuery(doc_type_query, db) %>%
     dplyr::filter(document_type == "extraction") %>%
     dplyr::pull(var = "source_table") %>%
     paste0("'", ., "'")
@@ -28,11 +29,20 @@ toxval_source_check_extraction_documents <- function(db, write_output=FALSE){
                                "WHERE source_table NOT IN (",
                                toString(active_extraction),
                                ") AND source_status='active'")
-
-  # Get list of all active tables without extraction document associations
   active_no_extraction = runQuery(no_extraction_query, db)
 
-  # Write sources without extraction document associations to Excel if needed, return values
-  if (write_output) {writexl::write_xlsx(active_no_extraction, "no_extraction_associations.xlsx")}
-  return(active_no_extraction)
+  # Query for documents w/o document type
+  doc_query = paste0("SELECT * FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
+                     "WHERE b.document_type='<NA>' AND a.source_table IN ",
+                     "(SELECT source_table FROM chemical_source_index WHERE source_status='active')")
+  null_docs = runQuery(doc_query, db)
+
+  # Write output to Excel if needed
+  if (write_output) {
+    writexl::write_xlsx(active_no_extraction, "no_extraction_associations.xlsx")
+    writexl::write_xlsx(null_docs, "null_documents.xlsx")
+  }
+
+  # Return sources without extraction association, documents without document_type
+  return(list(active_no_extraction, null_docs))
 }
