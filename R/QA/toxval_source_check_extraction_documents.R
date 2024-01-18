b6ad877948bbdba8a4ab7c4c9447b4c0e12b650d
Winter <Winter.Samuel@epa.gov>
2024-01-18 14:23:44 -0500

    Used source_hash to provide information for docs with null document_type

--- R/QA/toxval_source_check_extraction_documents.R
+++ R/QA/toxval_source_check_extraction_documents.R
@@ -9,17 +9,16 @@
 #' @import RMySQL dplyr readxl magrittr
 #--------------------------------------------------------------------------------------
 toxval_source_check_extraction_documents <- function(db, write_output=FALSE){
-
+  cat("Getting list of sources with no extraction document associations... ")
   # Query to check document type counts for each source
-  doc_type_query = paste0("SELECT DISTINCT a.source_table, b.document_type, count(*) AS n ",
-                          "FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
-                          "WHERE a.source_table IN ",
-                          "(SELECT source_table FROM chemical_source_index ",
-                          "WHERE source_status='active') ",
-                          "GROUP BY a.source_table, b.document_type")
+  doc_query = paste0("SELECT DISTINCT a.source_table, b.document_type, count(*) AS n ",
+                     "FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
+                     "WHERE a.source_table IN ",
+                     "(SELECT source_table FROM chemical_source_index WHERE source_status='active') ",
+                     "GROUP BY a.source_table, b.document_type")
 
   # Get list of all active tables with extraction document associations
-  active_extraction = runQuery(doc_type_query, db) %>%
+  active_extraction = runQuery(doc_query, db) %>%
     dplyr::filter(document_type == "extraction") %>%
     dplyr::pull(var = "source_table") %>%
     paste0("'", ., "'")
@@ -28,21 +27,44 @@ toxval_source_check_extraction_documents <- function(db, write_output=FALSE){
   no_extraction_query = paste0("SELECT source_table FROM chemical_source_index ",
                                "WHERE source_table NOT IN (",
                                toString(active_extraction),
-                               ") AND source_status='active'")
+                               ")")
   active_no_extraction = runQuery(no_extraction_query, db)
 
-  # Query for documents w/o document type
-  doc_query = paste0("SELECT * FROM documents_records a LEFT JOIN documents b on a.fk_doc_id = b.id ",
-                     "WHERE b.document_type='<NA>' AND a.source_table IN ",
-                     "(SELECT source_table FROM chemical_source_index WHERE source_status='active')")
-  null_docs = runQuery(doc_query, db)
+  # Record list of active sources without extraction document associations, if needed
+  if (write_output) {writexl::write_xlsx(active_no_extraction, "no_extraction_associations.xlsx")}
+  cat("Done\n")
 
-  # Write output to Excel if needed
-  if (write_output) {
-    writexl::write_xlsx(active_no_extraction, "no_extraction_associations.xlsx")
-    writexl::write_xlsx(null_docs, "null_documents.xlsx")
-  }
+  cat("Getting information for documents that are missing document_type field... ")
+  # Query source_hash, source_table, and document_type for active sources
+  doc_cat_info <- runQuery(paste0("SELECT distinct a.source_hash, a.source_table, b.document_type ",
+                                  "FROM documents_records a ",
+                                  "LEFT JOIN documents b on a.fk_doc_id = b.id ",
+                                  "WHERE a.source_table IN ",
+                                  "(SELECT source_table FROM chemical_source_index ",
+                                  "WHERE source_status='active')"), db)
 
-  # Return sources without extraction association, documents without document_type
-  return(list(active_no_extraction, null_docs))
+  # Get documents with null document_type
+  docs_missing_doc_type = lapply(unique(doc_cat_info$source_table), function(s_tbl){
+    # Check if table exists
+    s_check = runQuery(paste0("SHOW TABLES LIKE '", s_tbl, "'"), db)
+    if(!nrow(s_check)) return(NULL)
+
+    # Pull data for existing table
+    hashes = runQuery(paste0("SELECT DISTINCT source_hash FROM ", s_tbl), db)
+    doc_cat_info %>%
+
+      # Filter for existing documents with null document type
+      dplyr::filter(source_table == s_tbl,
+                    source_hash %in% hashes$source_hash,
+                    is.na(document_type)) %>%
+      return()
+  }) %>%
+    dplyr::bind_rows() %>%
+    dplyr::distinct()
+
+  # Record list of documents with null document_type, if needed
+  if (write_output) {writexl::write_xlsx(docs_missing_doc_type, "docs_missing_doc_type.xlsx")}
+  cat("Done")
+
+  return (list(active_no_extraction, docs_missing_doc_type))
 }
