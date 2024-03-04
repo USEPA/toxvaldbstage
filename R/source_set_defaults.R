22a5033801c2196336ea9319936a07735f205a23
Taylor Wall <wall.jonathan@epa.gov>
2024-03-04 15:26:05 -0500

    HotFix: Default character NA or blank to "-".

--- R/source_set_defaults.R
+++ R/source_set_defaults.R
@@ -1,26 +1,26 @@
 #--------------------------------------------------------------------------------------
-#' @description Set default value for NAs - jsut set NA to "-" for columns of type character
+#' @description Set default value for NAs - just set NA to "-" for columns of type character
 #' @param res The input dataframe
 #' @param source The data source name
 #' @return Returns the input dataframe with defaults set
 #' @title FUNCTION_TITLE
 #' @details DETAILS
-#' @examples 
+#' @examples
 #' \dontrun{
 #' if(interactive()){
 #'  #EXAMPLE1
 #'  }
 #' }
 #' @rdname source_set_defaults
-#' @export 
+#' @export
 #--------------------------------------------------------------------------------------
 source_set_defaults <- function(res,source) {
   printCurrentFunction(source)
   for(i in 1:ncol(res)) {
-    x = res[,i]
+    x = res %>% dplyr::pull(i)
     cc = class(x)
     if(any(cc == "character")) {
-      x[is.na(x)] = "-"
+      x[x %in% c(NA, "")] = "-"
       res[,i] = x
     }
   }
