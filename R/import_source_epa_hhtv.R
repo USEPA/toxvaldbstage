962800b895eed49c133030dc8b3395a2836e3af5
Winter <Winter.Samuel@epa.gov>
2024-06-04 11:23:53 -0400

    Added EPA HHTV import script, added fix.replace.unicode case, minor fixes to generic template

--- R/import_source_epa_hhtv.R
+++ R/import_source_epa_hhtv.R
@@ -1,12 +1,12 @@
 #--------------------------------------------------------------------------------------
-#' @description A generic template for adding data to toxval_source for a new source
+#' @description Push EPA HHTV data to toxval_source
 #'
 #' @param db The version of toxval_source into which the source is loaded.
 #' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
 #' @param do.reset If TRUE, delete data from the database for this source before
 #' @param do.insert If TRUE, insert data into the database, default FALSE
-#' @title FUNCTION_TITLE
-#' @return OUTPUT_DESCRIPTION
+#' @title import_source_epa_hhtv
+#' @return None; data is pushed to toxval_source
 #' @details DETAILS
 #' @examples
 #' \dontrun{
@@ -17,34 +17,38 @@
 #' @seealso
 #'  \code{\link[readxl]{read_excel}}
 #'  \code{\link[stringr]{str_trim}}
-#' @rdname import_generic_source
+#' @rdname import_source_epa_hhtv
 #' @export
 #' @importFrom readxl read_xlsx
 #' @importFrom stringr str_squish
 #' @importFrom dplyr mutate across where
 #' @importFrom tidyr replace_na
 #--------------------------------------------------------------------------------------
-import_generic_source <- function(db,chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
+import_source_epa_hhtv <- function(db, chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
   printCurrentFunction(db)
-  source = "name of the source"
-  source_table = "source_{source}"
+  source = "EPA HHTV"
+  source_table = "source_epa_hhtv"
   # Date provided by the source or the date the data was extracted
-  src_version_date = as.Date("YYYY-MM-DD")
-  dir = paste0(toxval.config()$datapath,"{source}/{source}_files/")
-  file = paste0(dir,"name of the source file.xlsx")
+  src_version_date = as.Date("2024-06-03")
+  dir = paste0(toxval.config()$datapath,"epa_hhtv/epa_hhtv_files/")
+  file = paste0(dir,"EPA HHTV extraction_2024-06-03.xlsx")
   res0 = readxl::read_xlsx(file)
   #####################################################################
   cat("Do any non-generic steps to get the data ready \n")
   #####################################################################
-  #
-  # the final file should have column names that include "name" and "casrn"
-  # additionally, the names in res need to match names in the source
-  # database table. You do not need to add any of the generic columns
-  # described in the SOP - they will get added in source_prep_and_load
-  #
 
-  # Add source specific transformations
-  res = res0
+  res = res0 %>%
+    dplyr::mutate(
+      # Add hardcoded fields
+      long_ref = study_reference,
+      year = last_updated,
+      subsource = document_name,
+      source_url = url,
+
+      # Match sex formatting to toxval
+      sex = sex %>%
+        gsub(", ", "/", .)
+    )
 
   # Standardize the names
   names(res) <- names(res) %>%
