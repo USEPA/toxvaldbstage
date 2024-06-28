49f2493659671085481fea1e40600697ea7251cb
Taylor Wall <wall.jonathan@epa.gov>
2024-06-28 16:26:29 -0400

    Updated source_url

--- R/import_ow_dwsha_source.R
+++ R/import_ow_dwsha_source.R
@@ -41,8 +41,8 @@ import_ow_dwsha_source <- function(db, chem.check.halt=FALSE, do.reset=FALSE, do
   res = res0 %>%
     dplyr::mutate(
       # Override URL from extraction document (broken link) - use URL from load script
-      source_url = "https://www.epa.gov/ground-water-and-drinking-water",
-      subsource_url = "https://www.epa.gov/system/files/documents/2022-01/dwtable2018.pdf",
+      source_url = "https://www.epa.gov/system/files/documents/2022-01/dwtable2018.pdf",
+      subsource_url = source_url,
 
       # Set study_type from RAC
       study_type = risk_assessment_class,
