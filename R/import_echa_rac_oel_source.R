66ad80727dcde56dce425796a13e10bb81419376
Taylor Wall <wall.taylor@epa.com>
2025-09-04 12:40:05 -0400

    Initial drafting

---
+++ R/import_echa_rac_oel_source.R
@@ -0,0 +1,87 @@
+#--------------------------------------------------------------------------------------
+#' @description Import of ECHA EAC OEL source into toxval_source
+#'
+#' @param db The version of toxval_source into which the source is loaded.
+#' @param chem.check.halt If TRUE and there are bad chemical names or casrn,
+#' @param do.reset If TRUE, delete data from the database for this source before
+#' @param do.insert If TRUE, insert data into the database, default FALSE
+#' @title FUNCTION_TITLE
+#' @return None; data is pushed to toxval_source
+#' @seealso
+#'  \code{\link[readxl]{read_excel}}
+#'  \code{\link[stringr]{str_trim}}
+#' @rdname import_generic_source
+#' @export
+#' @importFrom readxl read_xlsx
+#' @importFrom stringr str_squish
+#' @importFrom dplyr mutate across where
+#' @importFrom tidyr replace_na
+#--------------------------------------------------------------------------------------
+import_echa_rac_oel_source <- function(db, chem.check.halt=FALSE, do.reset=FALSE, do.insert=FALSE) {
+  printCurrentFunction(db)
+  source = "ECHA RAC OEL"
+  source_table = "source_echa_rac_oel"
+  # Date provided by the source or the date the data was extracted
+  src_version_date = as.Date("2025-01-10")
+  dir = paste0(toxval.config()$datapath,"echa_rac_oel/echa_rac_oel_files/")
+  file = paste0(dir, "ECHA_RAC_OELs_Derivation_2025.xlsx")
+  res0 = readxl::read_xlsx(file)
+  #####################################################################
+  cat("Do any non-generic steps to get the data ready \n")
+  #####################################################################
+  #
+  # the final file should have column names that include "name" and "casrn"
+  # additionally, the names in res need to match names in the source
+  # database table. You do not need to add any of the generic columns
+  # described in the SOP - they will get added in source_prep_and_load
+  #
+
+  # Add source specific transformations
+  res = res0 %>%
+    dplyr::mutate(year = summary_doc_year,
+                  casrn = dplyr::case_when(
+                    grepl("See Notes|NOCAS|example", casrn, ignore.case = TRUE) ~ NA,
+                    TRUE ~ casrn
+                  )) %>%
+    # Remove empty rows that only have NA values
+    .[rowSums(is.na(.)) < ncol(.), ] %>%
+    tidyr::separate_longer_delim(casrn, delim = "; ")
+
+  # Standardize the names
+  names(res) <- names(res) %>%
+    stringr::str_squish() %>%
+    # Replace whitespace and periods with underscore
+    gsub("[[:space:]]|[.]", "_", .) %>%
+    tolower()
+
+  res = res %>%
+    # Generic cleanup of strings before dedup check
+    dplyr::mutate(
+      dplyr::across(dplyr::where(is.character), ~tidyr::replace_na(., "-") %>%
+                      fix.replace.unicode() %>%
+                      stringr::str_squish()),
+      dplyr::across(dplyr::where(is.character), ~gsub("\\r|\\n|\\\\r|\\\\n", "", .)),
+      dplyr::across(dplyr::where(is.character), ~gsub("\\\\'", "'", .)),
+      dplyr::across(dplyr::where(is.character), ~gsub('\\\\\\"', '"', .))
+    )
+
+  # Fill blank hashing cols
+  res[, toxval.config()$hashing_cols[!toxval.config()$hashing_cols %in% names(res)]] <- "-"
+
+  # Perform deduping
+  res = toxval.source.import.dedup(res, hashing_cols=toxval.config()$hashing_cols)
+
+  # Add version date. Can be converted to a mutate statement as needed
+  res$source_version_date <- src_version_date
+  #####################################################################
+  cat("Prep and load the data\n")
+  #####################################################################
+  source_prep_and_load(db=db,
+                       source=source,
+                       table=source_table,
+                       res=res,
+                       do.reset=do.reset,
+                       do.insert=do.insert,
+                       chem.check.halt=chem.check.halt,
+                       hashing_cols=toxval.config()$hashing_cols)
+}
