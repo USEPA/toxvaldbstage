dfb9aee9315f1657a516bda3c4875b4587fbb08d
Taylor Wall <wall.taylor@epa.com>
2025-09-08 08:27:36 -0400

    Additional field clean-up and toxval_numeric range splitting

--- R/import_au_nhmrc_dwg_source.R
+++ R/import_au_nhmrc_dwg_source.R
@@ -36,12 +36,6 @@ import_au_nhmrc_dwg_source <- function(db, chem.check.halt=FALSE, do.reset=FALSE
   #####################################################################
   cat("Do any non-generic steps to get the data ready \n")
   #####################################################################
-  #
-  # the final file should have column names that include "name" and "casrn"
-  # additionally, the names in res need to match names in the source
-  # database table. You do not need to add any of the generic columns
-  # described in the SOP - they will get added in source_prep_and_load
-  #
 
   # Add source specific transformations
   res = res0
@@ -52,6 +46,105 @@ import_au_nhmrc_dwg_source <- function(db, chem.check.halt=FALSE, do.reset=FALSE
     # Replace whitespace and periods with underscore
     gsub("[[:space:]]|[.]", "_", .) %>%
     tolower()
+
+  res = res %>%
+    dplyr::mutate(
+      # Trimming the 'toxval_numeric' column to reduce trailing figures.
+      toxval_numeric = case_when(
+        toxval_numeric == "c" ~ NA,
+        toxval_numeric == "0.015/0.016" ~ "0.015-0.016",
+        toxval_numeric == "0.015 -0.055" ~ "0.015-0.055",
+        grepl("^[0-9.]+$", toxval_numeric) ~ format(signif(as.numeric(toxval_numeric), 5),
+                                                    scientific = TRUE,
+                                                    digits = 5,
+                                                    trim = TRUE),
+        TRUE ~ toxval_numeric
+    ) %>%
+      # Remove "f" character
+      gsub("f", "", .) %>%
+      stringr::str_squish(),
+    # Remove trailing ")"
+    casrn = casrn %>%
+      gsub("\\)$", "", .),
+    toxval_subtype = tolower(toxval_subtype),
+    # Fix exposure route/method/form
+    exposure_form = dplyr::case_when(
+      exposure_route == "feed" ~ exposure_route,
+      TRUE ~ exposure_form
+    ),
+    exposure_method = dplyr::case_when(
+      exposure_route %in% c("gavage", "drinking water", "diet") ~ exposure_route,
+      exposure_route == "feed" ~ "diet",
+      TRUE ~ exposure_method
+    ),
+    exposure_route = dplyr::case_when(
+      exposure_route %in% c("gavage", "drinking water", "diet", "feed") ~ "oral",
+    ),
+    # Fix sex
+    sex = dplyr::case_when(
+      sex == "M + F" ~ "male/female",
+      sex %in% c("F?", "F") ~ "female",
+      sex == "M" ~ "male",
+      TRUE ~ sex
+    ),
+    # Fix study_duration_units
+    study_duration_units = dplyr::case_when(
+      study_duration_units == "y" ~ "years",
+      study_duration_units == "d" ~ "days",
+      study_duration_units == "w" ~ "weeks",
+      study_duration_units %in% c("m", "month") ~ "months",
+      # Curator typo
+      study_duration_units == "k" ~ "weeks",
+      TRUE ~ study_duration_units
+    ),
+    # Fix toxval_units
+    toxval_units = toxval_units %>%
+      gsub("body weight per day|bodyweight per day", "bw/day", .)
+    ) %>%
+    # Filter out non-chemicals
+    dplyr::filter(!name %in% c("pH")) %>%
+    # Filter out records that do not have a name and casrn
+    dplyr::filter(!(is.na(name) & is.na(casrn))) %>%
+    tidyr::drop_na(toxval_type, toxval_numeric)
+
+  # Handle toxval_numeric range and relationship
+  res = res %>%
+    dplyr::mutate(range_flag = dplyr::case_when(
+      grepl("[0-9]-[0-9]", toxval_numeric) ~ 1,
+      TRUE ~ 0
+    ))
+
+  # Handle ranged toxval_numeric values
+  ranged_res = res %>%
+    dplyr::filter(range_flag == 1)
+
+  if(nrow(ranged_res)) {
+    ranged_res = ranged_res %>%
+      dplyr::mutate(
+        numeric_relationship_id = dplyr::row_number()
+      ) %>%
+      tidyr::separate(
+        col = toxval_numeric,
+        into = c("Lower Range", "Upper Range"),
+        sep = "-",
+        remove = TRUE
+      ) %>%
+      tidyr::pivot_longer(
+        cols = c("Lower Range", "Upper Range"),
+        values_to = "toxval_numeric",
+        names_to = "numeric_relationship_description"
+      )
+  } else {
+    # Empty dataframe with res cols to bind_rows()
+    ranged_res = res[0,]
+  }
+
+  # Add ranged data to res
+  res = res %>%
+    dplyr::filter(range_flag == 0) %>%
+    dplyr::bind_rows(ranged_res) %>%
+    dplyr::select(-range_flag) %>%
+    dplyr::mutate(toxval_numeric = as.numeric(toxval_numeric))
 
   res = res %>%
     # Generic cleanup of strings before dedup check
@@ -69,18 +162,6 @@ import_au_nhmrc_dwg_source <- function(db, chem.check.halt=FALSE, do.reset=FALSE
 
   # Perform deduping
   res = toxval.source.import.dedup(res, hashing_cols=toxval.config()$hashing_cols)
-
-  # Trimming the 'toxval_numeric' column to reduce trailing figures.
-  res = res %>%
-    dplyr::mutate(
-      toxval_numeric = case_when(
-      toxval_numeric == "c" ~ "-",
-      grepl("^[0-9.]+$", toxval_numeric) ~ format(signif(as.numeric(toxval_numeric), 5),
-      scientific = TRUE,
-      digits = 5,
-      trim = TRUE),
-      TRUE ~ toxval_numeric
-    ))
 
   # Add version date. Can be converted to a mutate statement as needed
   res$source_version_date <- src_version_date
